1dcab665d1c22aefadf39c61b439ed288930fe5f
GhitaB <ghitabizau27@gmail.com>
2023-02-01 15:46:36 +0200

    Refs #160116 - VideoView - add custom styles, fix video player spacing.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
+import './custom-styles.less';
 import superagent from 'superagent';
 import config from '@plone/volto/registry';
 import { addHeadersFactory } from '@plone/volto/helpers/Proxy/Proxy';
