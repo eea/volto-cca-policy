774253452ad7ec72340038af60eb35e7a0ea1853
GhitaB <ghitabizau27@gmail.com>
2023-02-10 15:44:35 +0200

    Refs #160116 - CaseStudyView - WIP add sections menu and sections.

--- src/components/theme/Views/CaseStudyView.jsx
+++ src/components/theme/Views/CaseStudyView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fragment } from 'react';
 import {
   HTMLField,
   ContentMetadata,
@@ -20,8 +21,100 @@ const PrimaryPhoto = (props) => {
   ) : null;
 };
 
+const dataDisplay = [
+  {
+    type: 'HTMLField',
+    field: 'stakeholder_participation',
+    section: 'stake_holder_anchor',
+    title: 'Stakeholder participation',
+  },
+  {
+    type: 'HTMLField',
+    field: 'success_limitations',
+    section: 'success_limitations_anchor',
+    title: 'Success and Limiting Factors',
+  },
+  {
+    type: 'HTMLField',
+    field: 'cost_benefit',
+    section: 'cost_benefit_anchor',
+    title: 'Costs and Benefits',
+  },
+  {
+    type: 'HTMLField',
+    field: 'legal_aspects',
+    section: 'legal_aspects',
+    title: 'Legal Aspects',
+  },
+  {
+    type: 'HTMLField',
+    field: 'implementation_time',
+    section: 'implementation_time_anchor',
+    title: 'Implementation Time',
+  },
+  {
+    type: 'HTMLField',
+    field: 'lifetime',
+    section: 'life_time_anchor',
+    title: 'Life Time',
+  },
+];
+
+const findSection = (title) => {
+  const found = dataDisplay.filter((item) => item.title === title);
+  if (found.length > 0) {
+    return found[0];
+  }
+  return null;
+};
+
+const sectionID = (title) => {
+  const found = findSection(title);
+  if (found === null) {
+    return title;
+  }
+  return found.section;
+};
+
+const SectionsMenu = (props) => {
+  const { sections } = props;
+
+  return (
+    <>
+      {sections.length > 0 && (
+        <>
+          <h5 className="Adaptation-option-selector">Additional Details</h5>
+          <ul>
+            {sections.map((data, index) => (
+              <li key={index}>
+                <a href={'#' + sectionID(data.title)}>{data.title}</a>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      <>
+        <h5 className="Adaptation-option-selector">Reference information</h5>
+        <ul>
+          <li>
+            <a href="#websites">Websites</a>
+          </li>
+          <li>
+            <a href="#source">Source</a>
+          </li>
+        </ul>
+      </>
+      <hr />
+    </>
+  );
+};
+
 function CaseStudyView(props) {
   const { content } = props;
+
+  const usedSections = dataDisplay.filter((data) =>
+    content.hasOwnProperty(data.field),
+  );
 
   return (
     <div className="case-study-view">
@@ -37,6 +130,24 @@ function CaseStudyView(props) {
                 value={content.long_description}
                 className="long_description"
               />
+
+              <SectionsMenu sections={usedSections} />
+
+              {usedSections.length > 0 && (
+                <>
+                  {usedSections.map((data, index) => (
+                    <Fragment key={index}>
+                      <div id={sectionID(data.title)} className="section">
+                        <h5 className="section-title">{data.title}</h5>
+                        <HTMLField
+                          value={content[data.field]}
+                          className="long_description"
+                        />
+                      </div>
+                    </Fragment>
+                  ))}
+                  </>
+                )}
               <h4>Challenges</h4>
               <HTMLField value={content.challenges} className="challenges" />
               <h4>Objectives</h4>
