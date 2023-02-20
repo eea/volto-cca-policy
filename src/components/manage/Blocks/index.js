f8ad2ad055c4ef3c1a545ace415be403b360f2e2
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-02-20 17:34:29 +0200

    Code cleanup

--- src/components/manage/Blocks/index.js
+++ src/components/manage/Blocks/index.js
@@ -1,11 +1,9 @@
 import { compose } from 'redux';
-import installLayoutSettings from './LayoutSettings';
 import installMKHMap from './MKHMap';
 
 export default function installBlocks(config) {
   config.blocks.blocksConfig.title.restricted = false;
   return compose(
-    installLayoutSettings,
     installMKHMap,
     //
   )(config);
