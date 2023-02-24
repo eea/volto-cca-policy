0a971c948d868250f4351b2906c8fd84babb79e4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-02-24 14:28:37 +0200

    WIP

--- src/components/manage/Blocks/ECDEIndicators/ECDEIndicator.jsx
+++ src/components/manage/Blocks/ECDEIndicators/ECDEIndicator.jsx
@@ -51,34 +51,30 @@ const CDS_TOOLBOX =
 // />
 const createIframe = (details_url) => {
   return `
-
-<!DOCTYPE html>
-<html>
-
-<head>
+  <iframe srcdoc="<head>
     <title>CDS integration test</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <script type="text/javascript">
+    <meta charset='utf-8' />
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+    <script type='text/javascript'>
         window.cds_toolbox = { cds_public_path: 'https://cds.climate.copernicus.eu/toolbox/' };
     </script>
-    <script type="text/javascript" src="https://cds.climate.copernicus.eu/toolbox/toolbox-latest.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
-        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script type='text/javascript' src='https://cds.climate.copernicus.eu/toolbox/toolbox-latest.js'></script>
+    <script src='https://code.jquery.com/jquery-3.6.3.min.js'
+        integrity='sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=' crossorigin='anonymous'></script>
 
 <body>
 
     <h1>Hello world</h1>
 
-    <select id="app-selection">
-        <option selected value="ecde-app-growing-degree-days">ECDE growing degree days</option>
-        <option value="ecde-app-mean-temperature">ECDE Mean Temperature</option>
+    <select id='app-selection'>
+        <option selected value='ecde-app-growing-degree-days'>ECDE growing degree days</option>
+        <option value='ecde-app-mean-temperature'>ECDE Mean Temperature</option>
     </select>
 
-    <div class="t-ct">
-        <div id="toolbox-app">
-            <div class="pre-app-loading">
-                <img src="/toolbox/assets/spinner.svg" alt="Loading">
+    <div class='t-ct'>
+        <div id='toolbox-app'>
+            <div class='pre-app-loading'>
+                <img src='/toolbox/assets/spinner.svg' alt='Loading'>
                 <div>
                     ...loading configuration...
                 </div>
@@ -109,7 +105,7 @@ const createIframe = (details_url) => {
                     if (value !== selectedApp) {
                         selectedApp = value;
                         window.cds_toolbox[ID].app_instance.destroy();
-                        window.cds_toolbox.runApp(ID, "https://cds.climate.copernicus.eu/workflows/c3s/" + selectedApp + "/master/configuration.json", {
+                        window.cds_toolbox.runApp(ID, 'https://cds.climate.copernicus.eu/workflows/c3s/' + selectedApp + '/master/configuration.json', {
                             workflowParams: {
                                 default: defaults[selectedApp]
                             }
@@ -121,9 +117,7 @@ const createIframe = (details_url) => {
 
     </script>
 
-</body>
-
-</html>`;
+</body>" />`;
 };
 
 export default function ECDEIndicator(props) {
