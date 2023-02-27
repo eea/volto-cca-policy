08b4c9f2af467d4a1f0a99cdf831441cd2ee6f28
GhitaB <ghitabizau27@gmail.com>
2023-02-27 13:10:03 +0200

    Refs #160935 - Add spinner.

--- src/components/manage/Blocks/ECDEIndicators/ECDEIndicator.jsx
+++ src/components/manage/Blocks/ECDEIndicators/ECDEIndicator.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { getContent } from '@plone/volto/actions';
+import spinner from '@eeacms/volto-cca-policy/../theme//assets/images/spinner.svg';
+import { Image } from 'semantic-ui-react';
 
 if (__CLIENT__) {
   window.cds_toolbox = {
@@ -21,7 +23,7 @@ function useIndicator(path) {
   );
 }
 
-const createIframe = (details_url, selected_region) => {
+const createIframe = (details_url, selected_region, spinner_url) => {
   let selected_app;
   // selected_region = 'Bayern';
 
@@ -47,7 +49,7 @@ const createIframe = (details_url, selected_region) => {
     <div class='t-ct'>
         <div id='toolbox-app'>
             <div class='pre-app-loading'>
-                <img src='/toolbox/assets/spinner.svg' alt='Loading'>
+                <img src='${spinner_url}' alt='Loading'>
                 <div>
                     ...loading configuration...
                 </div>
@@ -95,6 +97,8 @@ const createIframe = (details_url, selected_region) => {
 
 export default function ECDEIndicator(props) {
   const { indicatorUrl, selectedRegion } = props;
+  const [spinnerUrl, setSpinnerUrl] = useState(null);
+
   const indicator = useIndicator(indicatorUrl);
   // console.log('real indicator url', indicator?.details_app_toolbox_url);
 
@@ -103,12 +107,16 @@ export default function ECDEIndicator(props) {
   const details_url =
     'https://cds.climate.copernicus.eu/workflows/c3s/ecde-app-growing-degree-days/master/configuration.json';
 
+  React.useEffect(() => {
+    setSpinnerUrl(spinner);
+  }, []);
+
   return (
     <>
       {details_url ? (
         <div
           dangerouslySetInnerHTML={{
-            __html: createIframe(details_url, selectedRegion), // selectedRegion // 'Vest'
+            __html: createIframe(details_url, selectedRegion, spinnerUrl), // selectedRegion // 'Vest'
           }}
         />
       ) : null}
