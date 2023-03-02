a58d854b86684b8832710c92440f44cea0a5c06d
GhitaB <ghitabizau27@gmail.com>
2023-03-02 14:45:44 +0200

    Refs #160116 - PublicationReportView: improve layout.

--- src/components/theme/Views/PublicationReportView.jsx
+++ src/components/theme/Views/PublicationReportView.jsx
@@ -5,15 +5,21 @@ import {
   LinksList,
   PublishedModifiedInfo,
 } from '@eeacms/volto-cca-policy/helpers';
+import { Grid } from 'semantic-ui-react';
 
 function PublicationReportView(props) {
   const { content } = props;
   return (
     <div className="publication-report-view">
       <div className="ui container">
-        <div className="ui grid">
+        <Grid columns="12">
           <div className="row">
-            <div className="nine wide column left-col">
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={9}
+              className="col-left"
+            >
               <div className="ui label">Publications and Reports</div>
               <h1>{content.title}</h1>
               <h4>Description</h4>
@@ -49,14 +55,19 @@ function PublicationReportView(props) {
               )}
 
               <PublishedModifiedInfo {...props} />
-            </div>
-            <div className="three wide column right-col">
+            </Grid.Column>
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={3}
+              className="col-right"
+            >
               <div style={{}}>
                 <ContentMetadata {...props} />
               </div>
-            </div>
+            </Grid.Column>
           </div>
-        </div>
+        </Grid>
       </div>
     </div>
   );
