bc8d9604072d7532d99a9e47a40bc1fd14bd0d23
GhitaB <ghitabizau27@gmail.com>
2023-03-03 16:42:04 +0200

    Refs #160116 - GuidanceView: update layout.

--- src/components/theme/Views/GuidanceView.jsx
+++ src/components/theme/Views/GuidanceView.jsx
@@ -5,6 +5,7 @@ import {
   LinksList,
   PublishedModifiedInfo,
 } from '@eeacms/volto-cca-policy/helpers';
+import { Grid } from 'semantic-ui-react';
 
 const ContributorsList = (props) => {
   const { content } = props;
@@ -34,9 +35,14 @@ function GuidanceView(props) {
   return (
     <div className="guidance-view">
       <div className="ui container">
-        <div className="ui grid">
+        <Grid columns="12">
           <div className="row">
-            <div className="nine wide column left-col">
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={9}
+              className="col-left"
+            >
               <div className="ui label">Guidance Document</div>
               <h1>{content.title}</h1>
               <hr />
@@ -60,14 +66,19 @@ function GuidanceView(props) {
               )}
               <ContributorsList {...props} />
               <PublishedModifiedInfo {...props} />
-            </div>
-            <div className="three wide column right-col">
+            </Grid.Column>
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={3}
+              className="col-right"
+            >
               <div style={{}}>
                 <ContentMetadata {...props} />
               </div>
-            </div>
+            </Grid.Column>
           </div>
-        </div>
+        </Grid>
       </div>
     </div>
   );
