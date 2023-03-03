a46ae706781320d78c5708c4c00f9dbc079a6646
GhitaB <ghitabizau27@gmail.com>
2023-03-03 16:30:54 +0200

    Refs #160116 - AdaptationOptionView: update layout.

--- src/components/theme/Views/AdaptationOptionView.jsx
+++ src/components/theme/Views/AdaptationOptionView.jsx
@@ -5,6 +5,7 @@ import {
   LinksList,
   PublishedModifiedInfo,
 } from '@eeacms/volto-cca-policy/helpers';
+import { Grid } from 'semantic-ui-react';
 import { Fragment } from 'react';
 
 const dataDisplay = [
@@ -117,9 +118,14 @@ function AdaptationOptionView(props) {
   return (
     <div className="adaptation-option-view">
       <div className="ui container">
-        <div className="ui grid">
+        <Grid columns="12">
           <div className="row">
-            <div className="nine wide column left-col">
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={9}
+              className="col-left"
+            >
               <div className="ui label">Adaptation option</div>
               <h1>{content.title}</h1>
               <HTMLField
@@ -177,14 +183,19 @@ function AdaptationOptionView(props) {
               </div>
 
               <PublishedModifiedInfo {...props} />
-            </div>
-            <div className="three wide column right-col">
+            </Grid.Column>
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={3}
+              className="col-right"
+            >
               <div style={{}}>
                 <ContentMetadata {...props} />
               </div>
-            </div>
+            </Grid.Column>
           </div>
-        </div>
+        </Grid>
       </div>
     </div>
   );
