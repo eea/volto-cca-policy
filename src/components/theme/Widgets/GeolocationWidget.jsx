5aadc678b02b9ee6b913b661082309ab5c334500
GhitaB <ghitabizau27@gmail.com>
2023-04-06 11:44:32 +0300

    Refs #161509 - Add GeolocationWidget (WIP).

--- src/components/theme/Widgets/GeolocationWidget.jsx
+++ src/components/theme/Widgets/GeolocationWidget.jsx
@@ -5,6 +5,15 @@ import { TextArea } from 'semantic-ui-react';
 import { injectIntl } from 'react-intl';
 import { FormFieldWrapper } from '@plone/volto/components';
 
+import {
+  Controls,
+  Interactions,
+  Layer,
+  Map,
+  Layers,
+} from '@eeacms/volto-openlayers-map/api';
+import { openlayers as ol } from '@eeacms/volto-openlayers-map';
+
 const GeolocationWidget = (props) => {
   // const { id, value, onChange, placeholder } = props;
   const { id, value, placeholder } = props;
@@ -15,12 +24,57 @@ const GeolocationWidget = (props) => {
 
   const latitude = value.latitude;
   const longitude = value.longitude;
+  const [tileWMSSources, setTileWMSSources] = React.useState([]);
+
+  React.useEffect(() => {
+    setTileWMSSources([
+      new ol.source.TileWMS({
+        url: 'https://gisco-services.ec.europa.eu/maps/service',
+        params: {
+          LAYERS: 'OSMBlossomComposite',
+          TILED: true,
+        },
+        serverType: 'geoserver',
+        transition: 0,
+      }),
+    ]);
+  }, []); // ol.source.TileWMS, ol.source.Vector
+
+  if (__SERVER__) return '';
 
   return (
     <FormFieldWrapper {...props} className="geolocation-field">
       <h5>
         {latitude}, {longitude}
       </h5>
+      <div className="explore-sites-wrapper">
+        <div id="explore-sites">
+          <Map
+            view={{
+              center: ol.proj.fromLonLat([20, 50]),
+              showFullExtent: true,
+              zoom: 5,
+            }}
+            pixelRatio={1}
+            controls={ol.control.defaults({ attribution: false })}
+          >
+            <Layers>
+              <Controls attribution={false} zoom={false} />
+              <Interactions
+                doubleClickZoom={true}
+                dragAndDrop={false}
+                dragPan={true}
+                keyboardPan={true}
+                keyboardZoom={true}
+                mouseWheelZoom={true}
+                pointer={true}
+                select={false}
+              />
+              <Layer.Tile source={tileWMSSources[0]} zIndex={0} />
+            </Layers>
+          </Map>
+        </div>
+      </div>
       <TextArea
         id={`field-${id}`}
         name={id}
