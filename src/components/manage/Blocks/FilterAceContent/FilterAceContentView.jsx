3f8b97e62bb8bb5c93e4b26b61c982efdb5a47c7
kreafox <krisztina.n.elekes@gmail.com>
2023-05-03 14:23:25 +0300

    WIP filter ace content block

--- src/components/manage/Blocks/FilterAceContent/FilterAceContentView.jsx
+++ src/components/manage/Blocks/FilterAceContent/FilterAceContentView.jsx
@@ -1,34 +1,207 @@
 import React from 'react';
-// import { List } from 'semantic-ui-react';
-// import { Link } from 'react-router-dom';
-// import { flattenToAppURL } from '@plone/volto/helpers';
+import loadable from '@loadable/component';
 import ListingBody from '@plone/volto/components/manage/Blocks/Listing/ListingBody';
+import { useSelector, useDispatch } from 'react-redux';
+import { getVocabulary } from '@plone/volto/actions';
+import { OTHER_REGIONS } from '@eeacms/volto-cca-policy/helpers';
+import config from '@plone/volto/registry';
+
+import {
+  Option,
+  DropdownIndicator,
+  selectTheme,
+  customSelectStyles,
+} from '@plone/volto/components/manage/Widgets/SelectStyling';
+
+const Select = loadable(() => import('react-select'));
+
+const IMPACTS = 'eea.climateadapt.aceitems_climateimpacts';
+const SECTORS = 'eea.climateadapt.aceitems_sectors';
+
+const FIELDS = [
+  'bio_regions',
+  'countries',
+  'element_type',
+  'funding_programme',
+  'macro_regions',
+  'origin_website',
+  'search_text',
+  'search_type',
+  'sector',
+  'special_tags',
+];
+
+const fieldmap = {
+  origin_website: 'origin_website',
+  search_type: 'search_type',
+  element_type: 'elements',
+  search_text: 'SearchableText',
+  special_tags: 'special_tags',
+  sector: 'sectors',
+  countries: 'countries',
+  macro_regions: 'macro_regions',
+  bio_regions: 'bio_regions',
+  funding_programme: 'funding_programme',
+};
+
+const otherMacroRegions = [
+  'TRANS_BIO_MACARO',
+  'TRANS_MACRO_CAR_AREA',
+  'TRANS_MACRO_AMAZONIA',
+  'TRANS_BIO_ANATOLIAN',
+  'TRANS_MACRO_IND_OCEAN_AREA',
+];
+
+const impacts_no_value = [
+  {
+    label: 'All climate impacts',
+    value: '',
+  },
+];
+
+const sectors_no_value = [
+  {
+    label: 'All adaptation sectors',
+    value: '',
+  },
+];
+
+const applyQuery = (id, data, currentLang, impacts, sectors) => {
+  const defaultQuery = Object.entries(data)
+    .filter(
+      ([field, value]) => FIELDS.includes(field) && value && value.length > 0,
+    )
+    .map(([field, value]) => {
+      let values = [];
+      if (value.includes(OTHER_REGIONS)) {
+        values = [...value, ...otherMacroRegions];
+      }
+      values.splice(values.indexOf(OTHER_REGIONS), 1);
+
+      return {
+        i: fieldmap[field],
+        o: 'plone.app.querystring.operation.selection.any',
+        v: values,
+      };
+    });
+
+  defaultQuery.push({
+    i: 'Language',
+    o: 'plone.app.querystring.operation.selection.any',
+    v: currentLang,
+  });
+
+  if (impacts) defaultQuery.push(impacts);
+  if (sectors) defaultQuery.push(sectors);
+
+  return {
+    block: id,
+    limit: data.nr_items,
+    query: defaultQuery,
+    sort_on: data.sortBy,
+    sort_order: 'descending',
+  };
+};
 
 const FilterAceContentView = (props) => {
   const { data, id, mode = 'view' } = props;
-  //   const { title } = data;
-  //   const results = _v_results || [];
-  const queryData = {
-    block: id,
-    querystring: {
-      query: [
-        {
-          i: 'portal_type',
-          o: 'plone.app.querystring.operation.selection.any',
-          v: ['Document'],
-        },
-      ],
-    },
-    sort_order: 'ascending',
-  };
+  const dispatch = useDispatch();
+  const currentLang = useSelector((state) => state.intl.locale);
+  const impactsVocabItems = useSelector((state) =>
+    state.vocabularies[IMPACTS]?.loaded
+      ? state.vocabularies[IMPACTS].items
+      : [],
+  );
+  const sectorsVocabItems = useSelector((state) =>
+    state.vocabularies[SECTORS]?.loaded
+      ? state.vocabularies[SECTORS].items
+      : [],
+  );
+
+  const [impactsQuery, setImpactsQueryQuery] = React.useState();
+  const [sectorsQuery, setSectorsQuery] = React.useState();
+
+  React.useEffect(() => {
+    const action = getVocabulary({
+      vocabNameOrURL: IMPACTS,
+    });
+    dispatch(action);
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    const action = getVocabulary({
+      vocabNameOrURL: SECTORS,
+    });
+    dispatch(action);
+  }, [dispatch]);
+
+  console.log('data', data);
+
+  const listingBodyData = applyQuery(
+    id,
+    data,
+    currentLang,
+    impactsQuery,
+    sectorsQuery,
+  );
+  const { variations } = config.blocks.blocksConfig.listing;
+
+  console.log('listing variations', variations);
+
+  // console.log(variations.filter((v) => v.id === 'summary'));
 
   return (
     <div className="block filter-acecontent-block">
-      Filter ace content view
       {data.title && <h4>{data.title}</h4>}
+      <Select
+        id="field-impacts"
+        name="impacts"
+        disabled={false}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        options={[impacts_no_value[0], ...(impactsVocabItems || [])]}
+        styles={customSelectStyles}
+        theme={selectTheme}
+        components={{ DropdownIndicator, Option }}
+        defaultValue={impacts_no_value}
+        onChange={({ value }) => {
+          if (value) {
+            setImpactsQueryQuery({
+              i: 'climate_impacts',
+              o: 'plone.app.querystring.operation.selection.any',
+              v: value,
+            });
+          } else {
+            setImpactsQueryQuery(null);
+          }
+        }}
+      />
+      <Select
+        id="field-sectors"
+        name="sectors"
+        disabled={false}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        options={[sectors_no_value[0], ...(sectorsVocabItems || [])]}
+        styles={customSelectStyles}
+        theme={selectTheme}
+        components={{ DropdownIndicator, Option }}
+        defaultValue={sectors_no_value}
+        onChange={({ value }) => {
+          if (value) {
+            setSectorsQuery({
+              i: 'sectors',
+              o: 'plone.app.querystring.operation.selection.any',
+              v: value,
+            });
+          } else {
+            setSectorsQuery(null);
+          }
+        }}
+      />
       <ListingBody
         id={id}
-        data={queryData}
+        data={listingBodyData}
         path={props.path}
         isEditMode={mode === 'edit'}
         variation={null}
--- src/components/manage/Blocks/FilterAceContent/schema.js
+++ src/components/manage/Blocks/FilterAceContent/schema.js
@@ -1,112 +1,116 @@
-export default {
-  title: 'Filter Ace Content',
-  fieldsets: [
-    {
-      id: 'default',
-      title: 'Default',
-      fields: [
-        'title',
-        'search_text',
-        'origin_website',
-        'search_type',
-        'element_type',
-        'sector',
-        'special_tags',
-        'countries',
-        'macro_regions',
-        'bio_regions',
-        'funding_programme',
-        'nr_items',
-      ],
-    },
-  ],
-  properties: {
-    title: {
-      title: 'Block title',
-    },
-    search_text: {
-      title: 'Search text',
-    },
-    origin_website: {
-      title: 'Origin website',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.funding_programme',
+const FilterSchema = (data, macro_regions) => {
+  console.log('macro_regions', macro_regions);
+
+  return {
+    title: 'Filter Ace Content',
+    fieldsets: [
+      {
+        id: 'default',
+        title: 'Default',
+        fields: [
+          'title',
+          'search_text',
+          'origin_website',
+          'search_type',
+          'element_type',
+          'sector',
+          'special_tags',
+          'countries',
+          'macro_regions',
+          'bio_regions',
+          'funding_programme',
+          'nr_items',
+        ],
+      },
+    ],
+    properties: {
+      title: {
+        title: 'Block title',
+      },
+      search_text: {
+        title: 'Search text',
+      },
+      origin_website: {
+        title: 'Origin website',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.origin_website',
+        },
+      },
+      search_type: {
+        title: 'Aceitem type',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.search_types_vocabulary',
+        },
+      },
+      element_type: {
+        title: 'Element type',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.element_types_vocabulary',
+        },
+      },
+      sector: {
+        title: 'Sector',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.aceitems_sectors',
+        },
+      },
+      special_tags: {
+        title: 'Special tags',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.special_tags',
+        },
+      },
+      countries: {
+        title: 'Countries',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.ace_countries',
+        },
+      },
+      macro_regions: {
+        title: 'Macro-Transnational Regions',
+        widget: 'array',
+        choices: macro_regions,
+      },
+      bio_regions: {
+        title: 'Biogeographical Regions',
+        widget: 'array',
+        vocabulary: {
+          '@id': 'eea.climateadapt.bioregions',
+        },
+      },
+      funding_programme: {
+        title: 'Funding programmes',
+        type: 'choice',
+        vocabulary: {
+          '@id': 'eea.climateadapt.funding_programme',
+        },
+      },
+      nr_items: {
+        title: 'Nr of items to show',
+        type: 'number',
+        default: '0',
+      },
+      sortBy: {
+        title: 'Sort by',
+        description: 'Sort order for results and assigned items',
+        choices: [
+          ['modified', 'Last Modified'],
+          ['effective', 'Last Published'],
+          ['getId', 'Alphabetical sorting'],
+        ],
+        // vocabulary: {
+        //   '@id': 'sortby_vocabulary',
+        // },
       },
     },
-    search_type: {
-      title: 'Aceitem type',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.search_types_vocabulary',
-      },
-    },
-    element_type: {
-      title: 'Element type',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.element_types_vocabulary',
-      },
-    },
-    sector: {
-      title: 'Sector',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.aceitems_sectors',
-      },
-    },
-    special_tags: {
-      title: 'Special tags',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.special_tags',
-      },
-    },
-    countries: {
-      title: 'Countries',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.ace_countries',
-      },
-    },
-    macro_regions: {
-      title: 'Macro-Transnational Regions',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.regions',
-      },
-    },
-    bio_regions: {
-      title: 'Biogeographical Regions',
-      widget: 'array',
-      vocabulary: {
-        '@id': 'eea.climateadapt.bioregions',
-      },
-    },
-    funding_programme: {
-      title: 'Funding programmes',
-      type: 'choice',
-      vocabulary: {
-        '@id': 'eea.climateadapt.funding_programme',
-      },
-    },
-    nr_items: {
-      title: 'Nr of items to show',
-      type: 'number',
-      default: '0',
-    },
-    sortBy: {
-      title: 'Sort by',
-      description: 'Sort order for results and assigned items',
-      choices: [
-        ['MODIFIED', 'Last Modified'],
-        ['EFFECTIVE', 'Last Published'],
-        ['NAME', 'Alphabetical sorting'],
-      ],
-      // vocabulary: {
-      //   '@id': 'sortby_vocabulary',
-      // },
-    },
-  },
-  required: [],
+    required: [],
+  };
 };
+
+export default FilterSchema;
