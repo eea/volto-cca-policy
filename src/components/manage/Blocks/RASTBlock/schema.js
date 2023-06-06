8c4668bbad499ff4690861d4fbb73f86f929a8cd
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-06-06 15:21:35 +0300

    WIP on rast block

--- src/components/manage/Blocks/RASTBlock/schema.js
+++ src/components/manage/Blocks/RASTBlock/schema.js
@@ -1,3 +1,5 @@
+const fields = ['step_1', 'step_2', 'step_3', 'step_4', 'step_5', 'step_6'];
+
 export default {
   title: 'RAST',
 
@@ -5,11 +7,42 @@ export default {
     {
       id: 'default',
       title: 'Default',
-      fields: [],
+      fields,
     },
   ],
 
-  properties: {},
+  properties: {
+    step_1: {
+      title: 'Step 1',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+    step_2: {
+      title: 'Step 2',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+    step_3: {
+      title: 'Step 3',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+    step_4: {
+      title: 'Step 4',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+    step_5: {
+      title: 'Step 5',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+    step_6: {
+      title: 'Step 6',
+      widget: 'object_browser',
+      mode: 'link',
+    },
+  },
 
   required: [],
 };
