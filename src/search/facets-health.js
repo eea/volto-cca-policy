6695897b6e70b3d3708e2ef895b5bd5a9b2fe9f5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-09-04 13:03:26 +0300

    Also fix health catalog

--- src/search/facets-health.js
+++ src/search/facets-health.js
@@ -1,3 +1,6 @@
+import { booleanFacet } from '@eeacms/search';
+import { getTodayWithTime } from './utils';
+
 const special = [
   {
     field: 'issued.date',
@@ -53,43 +56,32 @@ const special = [
     },
   },
 
-  {
+  booleanFacet(() => ({
     field: 'IncludeArchived',
     label: 'Include archived content',
-    on: null,
+    id: 'archived-facet',
+    showInFacetsList: false,
+    showInSecondaryFacetsList: true,
+    isFilter: true, // filters don't need facet options to show up
+
+    // we want this to be applied by default
+    // when the facet is checked, then apply the `on` key:
     off: {
       constant_score: {
         filter: {
           bool: {
             should: [
-              {
-                bool: {
-                  must_not: {
-                    exists: {
-                      field: 'expires',
-                    },
-                  },
-                },
-              },
-              {
-                range: {
-                  expires: {
-                    gte: '2023-04-27T11:52:29Z',
-                  },
-                },
-              },
+              { bool: { must_not: { exists: { field: 'expires' } } } },
+              // Functions should be supported in the buildFilters
+              { range: { expires: { gte: getTodayWithTime() } } },
             ],
           },
         },
       },
     },
-    factory: 'BooleanFacet',
-    wrapper: 'DummySUIFacetWrapper',
-    showInFacetsList: false,
-    id: 'archived-facet',
-    showInSecondaryFacetsList: true,
-    isFilter: true,
-  },
+    on: null,
+  })),
+
   {
     field: 'op_cluster',
     factory: 'MultiTermFacet',
