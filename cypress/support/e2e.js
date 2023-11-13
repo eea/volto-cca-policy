1434f2624035572056633c0eb2598585c7e04722
Tripon Eugen <tripon.eugen@gmail.com>
2023-11-13 12:57:55 +0200

    Refs #259267 - test cypress config

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,7 +22,7 @@ import './commands';
 import '@cypress/code-coverage/support';
 
 // Fail Fast
-import "cypress-fail-fast";
+// import "cypress-fail-fast";
 
 export const slateBeforeEach = (contentType = 'Document') => {
   cy.autologin();
@@ -39,7 +39,7 @@ export const slateBeforeEach = (contentType = 'Document') => {
   });
   cy.visit('/cypress/my-page');
   cy.waitForResourceToLoad('@navigation');
-  cy.waitForResourceToLoad('@breadcrumbs');
+  // cy.waitForResourceToLoad('@breadcrumbs');
   cy.waitForResourceToLoad('@actions');
   cy.waitForResourceToLoad('@types');
   cy.waitForResourceToLoad('my-page');
