1e803e5bd3d3fb7558f261c76c68866be7beb8b5
Tripon Eugen <tripon.eugen@gmail.com>
2023-12-04 14:25:25 +0200

    Refs #260715 rast-block updates

---
+++ src/components/manage/Blocks/RASTBlock/ContextNavigation.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { compose } from 'redux';
+import { withRouter } from 'react-router';
+
+import RASTMap from './RASTMap';
+import RASTAccordion from './RASTAccordion';
+
+import { withContentNavigation } from '@plone/volto/components/theme/Navigation/withContentNavigation';
+
+import { useLocation } from 'react-router-dom';
+
+/**
+ * A navigation slot implementation, similar to the classic Plone navigation
+ * portlet. It uses the same API, so the options are similar to
+ * INavigationPortlet
+ */
+export function ContextNavigationComponent(props) {
+  const { navigation = {} } = props;
+  const { items = [] } = navigation;
+  let activeMenu = null;
+
+  const location = useLocation();
+  location.pathname;
+  for (let i = 0; i < items.length; i++) {
+    let itemUrl = '/' + items[i].href.split('/').slice(3).join('/');
+    items[i].is_active = false;
+    if (location.pathname.includes(itemUrl)) {
+      activeMenu = i;
+      items[i].is_active = true;
+    }
+  }
+
+  return items.length ? (
+    <>
+      <RASTMap
+        items={items}
+        pathname={location.pathname}
+        activeMenu={activeMenu}
+      />
+      <RASTAccordion datasets={items} activeMenu={activeMenu} />
+    </>
+  ) : (
+    ''
+  );
+}
+
+export default compose(
+  withRouter,
+  withContentNavigation,
+)(ContextNavigationComponent);
---
+++ src/components/manage/Blocks/RASTBlock/RASTAccordion.jsx
@@ -0,0 +1,80 @@
+import { Accordion, Icon } from 'semantic-ui-react';
+import RASTAccordionContent from './RASTAccordionContent';
+
+const RASTAccordion = (props) => {
+  const { datasets = {}, activeMenu } = props;
+
+  const [activeIndex, setActiveIndex] = React.useState([activeMenu]);
+
+  const handleActiveIndex = (index) => {
+    setActiveIndex(index);
+  };
+  const handleClick = (e, titleProps) => {
+    const { index, id, item } = titleProps;
+
+    const newIndex =
+      activeIndex.indexOf(index) === -1
+        ? [...activeIndex, index]
+        : activeIndex.filter((item) => item !== index);
+
+    handleActiveIndex(newIndex);
+  };
+  const isActive = (id) => {
+    return activeIndex.includes(id);
+  };
+  return (
+    <>
+      {datasets.map((dataset, index) => {
+        const { id } = dataset;
+        const active = isActive(index);
+        let datasetPath = '/' + dataset.href.split('/').slice(3).join('/');
+
+        return (
+          <>
+            <Accordion id={id} key={index} className="secondary">
+              <Accordion.Title
+                role="button"
+                tabIndex={0}
+                active={active}
+                aria-expanded={active}
+                index={index}
+                onClick={(e) => handleClick(e, { index, id, dataset })}
+                onKeyDown={(e) => {
+                  if (e.keyCode === 13 || e.keyCode === 32) {
+                    e.preventDefault();
+                    handleClick(e, { index, id, dataset });
+                  }
+                }}
+              >
+                <span className="dataset-title">{dataset.title}</span>
+                {active ? (
+                  <Icon className="ri-arrow-up-s-line" />
+                ) : (
+                  <Icon className="ri-arrow-down-s-line" />
+                )}
+              </Accordion.Title>
+              <Accordion.Content active={active}>
+                <RASTAccordionContent
+                  key={index}
+                  params={{
+                    name: 'CurrentTitle',
+                    includeTop: false,
+                    currentFolderOnly: true,
+                    topLevel: 3,
+                    bottomLevel: 6,
+                    rootPath: datasetPath,
+                    title: dataset.title,
+                  }}
+                  location={{ pathname: datasetPath }}
+                  main={{ title: dataset.title, href: dataset['@id'] }}
+                />
+              </Accordion.Content>
+            </Accordion>
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+export default RASTAccordion;
