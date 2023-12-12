7125d5d79a31cba7c0f00929260e2c80e323896b
Tripon Eugen <tripon.eugen@gmail.com>
2023-12-12 14:38:38 +0200

    Refs #260715 - rast block open current path

--- src/components/manage/Blocks/RASTBlock/ContextNavigation.jsx
+++ src/components/manage/Blocks/RASTBlock/ContextNavigation.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { compose } from 'redux';
-import { withRouter } from 'react-router';
 
 import RASTMap from './RASTMap';
 import RASTAccordion from './RASTAccordion';
+import { useLocation } from 'react-router-dom';
 
 import { withContentNavigation } from '@plone/volto/components/theme/Navigation/withContentNavigation';
 
@@ -17,10 +17,11 @@ export function ContextNavigationComponent(props) {
   const { items = [] } = navigation;
   let activeMenu = null;
 
+  const curent_location = useLocation();
   for (let i = 0; i < items.length; i++) {
     let itemUrl = '/' + items[i].href.split('/').slice(3).join('/');
     items[i].is_active = false;
-    if (location.pathname.includes(itemUrl)) {
+    if (curent_location.pathname.includes(itemUrl)) {
       activeMenu = i;
       items[i].is_active = true;
     }
