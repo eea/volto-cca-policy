7df5146e0f6b27accf8fd5a5964f5454c078900a
Tripon Eugen <tripon.eugen@gmail.com>
2024-01-29 20:46:09 +0200

    Refs #161485 - filter test for sonarqube

---
+++ src/components/manage/Blocks/CountryMapHeatIndex/Filter.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import Filter from './Filter';
+
+const mockStore = configureStore();
+
+describe('Filter', () => {
+  it('should render the component', () => {
+    const data = {
+      thematicMapMode: 'hhap',
+      setThematicMapMode: function() {return 'hhap'} 
+    };
+
+    const store = mockStore({
+      userSession: { token: '1234' },
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Filter {...data} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(container.querySelector('#cse-filter')).toBeInTheDocument();
+  });
+});
