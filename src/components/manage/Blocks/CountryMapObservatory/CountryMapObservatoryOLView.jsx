b732204a510a3301a021c7cf5225bcbe414b87cd
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-11 10:04:45 +0200

    Split styles into separate thing

--- src/components/manage/Blocks/CountryMapObservatory/CountryMapObservatoryOLView.jsx
+++ src/components/manage/Blocks/CountryMapObservatory/CountryMapObservatoryOLView.jsx
@@ -15,6 +15,75 @@ const url =
 
 let highlight = null; // easy global
 
+const makeStyles = () => {
+  const fill = new ol.style.Fill();
+  const stroke = new ol.style.Stroke({
+    // color: 'rgba(255,255,255,0.8)',
+    color: '#A0A0A0',
+    width: 2,
+  });
+
+  const overlayStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#A0A0A0',
+      width: 2,
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgb(1, 112, 183, 0.8)',
+    }),
+    renderer: function (pixelCoordinates, state) {
+      const context = state.context;
+      const geometry = state.geometry.clone();
+      geometry.setCoordinates(pixelCoordinates);
+      const extent = geometry.getExtent();
+      const width = ol.extent.getWidth(extent);
+      const height = ol.extent.getHeight(extent);
+      const flag = state.feature.get('flag');
+      if (!flag || height < 1 || width < 1) {
+        return;
+      }
+
+      // Stitch out country shape from the blue canvas
+      context.save();
+      const renderContext = ol.render.toContext(context, {
+        pixelRatio: 1,
+      });
+      renderContext.setFillStrokeStyle(fill, stroke);
+      renderContext.drawGeometry(geometry);
+      context.clip();
+
+      // Fill transparent country with the flag image
+      const bottomLeft = ol.extent.getBottomLeft(extent);
+      const left = bottomLeft[0];
+      const bottom = bottomLeft[1];
+      context.drawImage(flag, left, bottom, width, height);
+      context.restore();
+    },
+  });
+
+  const emptyStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'rgb(1, 1, 1, 0)',
+      width: 1,
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgb(1, 1, 1, 0)',
+    }),
+  });
+
+  const eucountriesStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#A0A0A0',
+      width: 1,
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgb(1, 112, 183, 0.3)',
+    }),
+  });
+
+  return { eucountriesStyle, emptyStyle, overlayStyle };
+};
+
 const tooltipStyle = {
   position: 'relative',
   zIndex: 2,
@@ -32,6 +101,7 @@ const tooltipStyle = {
 const Interactions = ({ overlaySource, tooltipRef, baseUrl }) => {
   const history = useHistory();
   const map = useMapContext().map;
+
   React.useEffect(() => {
     if (!map) return;
     map.on('pointermove', function (evt) {
@@ -125,70 +195,7 @@ const CountryMapObservatoryView = (props) => {
     ]);
   }, []);
 
-  const fill = new ol.style.Fill();
-  const stroke = new ol.style.Stroke({
-    // color: 'rgba(255,255,255,0.8)',
-    color: '#A0A0A0',
-    width: 2,
-  });
-
-  const overlayStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: '#A0A0A0',
-      width: 2,
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgb(1, 112, 183, 0.8)',
-    }),
-    renderer: function (pixelCoordinates, state) {
-      const context = state.context;
-      const geometry = state.geometry.clone();
-      geometry.setCoordinates(pixelCoordinates);
-      const extent = geometry.getExtent();
-      const width = ol.extent.getWidth(extent);
-      const height = ol.extent.getHeight(extent);
-      const flag = state.feature.get('flag');
-      if (!flag || height < 1 || width < 1) {
-        return;
-      }
-
-      // Stitch out country shape from the blue canvas
-      context.save();
-      const renderContext = ol.render.toContext(context, {
-        pixelRatio: 1,
-      });
-      renderContext.setFillStrokeStyle(fill, stroke);
-      renderContext.drawGeometry(geometry);
-      context.clip();
-
-      // Fill transparent country with the flag image
-      const bottomLeft = ol.extent.getBottomLeft(extent);
-      const left = bottomLeft[0];
-      const bottom = bottomLeft[1];
-      context.drawImage(flag, left, bottom, width, height);
-      context.restore();
-    },
-  });
-
-  const emptyStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgb(1, 1, 1, 0)',
-      width: 1,
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgb(1, 1, 1, 0)',
-    }),
-  });
-
-  const eucountriesStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: '#A0A0A0',
-      width: 1,
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgb(1, 112, 183, 0.3)',
-    }),
-  });
+  const styles = React.useMemo(makeStyles, []);
 
   const tooltipRef = React.useRef();
 
@@ -211,13 +218,21 @@ const CountryMapObservatoryView = (props) => {
             baseUrl={props.path || props.location?.pathname || ''}
           />
         )}
-        <Layer.Vector source={overlaySource} zIndex={3} style={overlayStyle} />
+        <Layer.Vector
+          source={overlaySource}
+          zIndex={3}
+          style={styles.overlayStyle}
+        />
         <Layer.Vector
           source={euCountriesSource}
           zIndex={2}
-          style={eucountriesStyle}
+          style={styles.eucountriesStyle}
         />
-        <Layer.Vector source={rectsSource} zIndex={1} style={emptyStyle} />
+        <Layer.Vector
+          source={rectsSource}
+          zIndex={1}
+          style={styles.emptyStyle}
+        />
         <Layer.Tile source={tileWMSSources[0]} zIndex={0} />
       </Layers>
     </Map>
