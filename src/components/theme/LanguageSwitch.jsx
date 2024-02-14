1908fca7707f526081a449f79eae6bdd6350b886
kreafox <krisztina.n.elekes@gmail.com>
2024-02-14 13:41:38 +0200

    Cleanup code

--- src/components/theme/LanguageSwitch.jsx
+++ src/components/theme/LanguageSwitch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from '@eeacms/volto-eea-design-system/ui';
 import { find } from 'lodash';
 import globeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/global-line.svg';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import config from '@plone/volto/registry';
 import { Dropdown, Image } from 'semantic-ui-react';
 import { flattenToAppURL } from '@plone/volto/helpers';
@@ -10,13 +10,11 @@ import { flattenToAppURL } from '@plone/volto/helpers';
 // dispatch(changeLanguage(redirectToLanguage, locale.default));
 export default function LanguageSwitch({ history }) {
   const { eea } = config.settings;
-  const dispatch = useDispatch();
   const translations = useSelector(
     (state) => state.content.data?.['@components']?.translations?.items,
   );
   const width = useSelector((state) => state.screen?.width);
 
-  console.log('translations', translations);
   const currentLang = useSelector((state) => state.intl.locale);
   const [language, setLanguage] = React.useState(
     currentLang || eea.defaultLanguage,
