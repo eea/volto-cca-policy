b5dc1fe0fa28ff05dc2482d23fa7b0c4b27941de
kreafox <krisztina.n.elekes@gmail.com>
2024-02-19 12:02:00 +0200

    fix: typo

--- src/customizations/volto/components/theme/Widgets/RichTextWidget.jsx
+++ src/customizations/volto/components/theme/Widgets/RichTextWidget.jsx
@@ -1,6 +1,6 @@
 // Original: https://github.com/plone/volto/blob/16.x.x/src/components/theme/Widgets/RichTextWidget.jsx
 // Fixed 'dangerouslySetInnerHTML' did not match. Server: "" Client: "<p>This is my paragraph.</p>" error
-// replaced <p> to <div>, the value can already be wrapped in <p> and nested <p> in invalid HTML
+// replaced <p> to <div>, the value can already be wrapped in <p> and nested <p> is invalid HTML
 
 import React from 'react';
 import cx from 'classnames';
