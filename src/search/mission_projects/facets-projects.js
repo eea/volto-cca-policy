10bbb14a12d8450295470536170979874bf5d0f5
kreafox <krisztina.n.elekes@gmail.com>
2024-02-19 16:46:50 +0200

    change: update mission projects facets

--- src/search/mission_projects/facets-projects.js
+++ src/search/mission_projects/facets-projects.js
@@ -3,19 +3,26 @@ import { multiTermFacet } from '@eeacms/search';
 import globalSearchBaseConfig from '@eeacms/volto-globalsearch/config/global-search-base-config.js';
 
 const facets = [
-  ...globalSearchBaseConfig.facets,
   multiTermFacet({
-    field: 'cca_adaptation_sectors.keyword',
+    field: 'cca_funding_programme.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Sectors',
+    label: 'Funding Programme',
+    iconsFamily: 'Funding Programme',
     alwaysVisible: false,
   }),
   multiTermFacet({
     field: 'cca_climate_impacts.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Climate impacts',
+    label: 'Climate Impacts',
+    alwaysVisible: false,
+  }),
+  multiTermFacet({
+    field: 'cca_adaptation_sectors.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Adaptation Sectors',
     alwaysVisible: false,
   }),
   multiTermFacet({
@@ -25,6 +32,7 @@ const facets = [
     label: 'Adaptation Elements',
     alwaysVisible: false,
   }),
+  ...globalSearchBaseConfig.facets,
 ];
 
 export default facets;
