ad7a023c05933d855329dbe9c1638ec24aaab2ab
kreafox <krisztina.n.elekes@gmail.com>
2024-02-19 16:30:28 +0200

    change: facets order

--- src/search/mission_stories/facets-stories.js
+++ src/search/mission_stories/facets-stories.js
@@ -3,15 +3,14 @@ import { multiTermFacet } from '@eeacms/search';
 import globalSearchBaseConfig from '@eeacms/volto-globalsearch/config/global-search-base-config.js';
 
 const facets = [
-  ...globalSearchBaseConfig.facets,
-  // multiTermFacet({
-  //   field: 'spatial',
-  //   isFilterable: false,
-  //   isMulti: true,
-  //   label: 'Country',
-  //   iconsFamily: 'Country',
-  //   alwaysVisible: false,
-  // }),
+  multiTermFacet({
+    field: 'cca_climate_impacts.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Climate Impacts',
+    iconsFamily: 'Climate Impacts',
+    alwaysVisible: false,
+  }),
   multiTermFacet({
     field: 'cca_adaptation_sectors.keyword',
     isFilterable: false,
@@ -28,28 +27,21 @@ const facets = [
     iconsFamily: 'Key Community Systems',
     alwaysVisible: false,
   }),
-  multiTermFacet({
-    field: 'climate_threats.keyword',
-    isFilterable: false,
-    isMulti: true,
-    label: 'Hazard Type',
-    iconsFamily: 'Hazard Type',
-    alwaysVisible: false,
-  }),
-  multiTermFacet({
-    field: 'cca_climate_impacts.keyword',
-    isFilterable: false,
-    isMulti: true,
-    label: 'Climate Impacts',
-    iconsFamily: 'Climate Impacts',
-    alwaysVisible: false,
-  }),
+  ...globalSearchBaseConfig.facets,
   multiTermFacet({
     field: 'cca_funding_programme.keyword',
     isFilterable: false,
     isMulti: true,
     label: 'Funding Programme',
     iconsFamily: 'Funding Programme',
+    alwaysVisible: false,
+  }),
+  multiTermFacet({
+    field: 'climate_threats.keyword',
+    isFilterable: false,
+    isMulti: true,
+    label: 'Hazard Type',
+    iconsFamily: 'Hazard Type',
     alwaysVisible: false,
   }),
 ];
