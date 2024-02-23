4660adc38a9cc7d0f34db021918e6148f1aef367
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-23 16:03:33 +0200

    Show tags in news item

--- src/components/theme/Views/NewsItemView.jsx
+++ src/components/theme/Views/NewsItemView.jsx
@@ -1,7 +1,30 @@
+import { When } from '@plone/volto/components/theme/View/EventDatesInfo';
 import React from 'react';
 import { HTMLField } from '@eeacms/volto-cca-policy/helpers';
 import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
 import config from '@plone/volto/registry';
+import { Label } from 'semantic-ui-react';
+
+const Date = (props) => {
+  const date = props.content?.effective;
+  return date ? (
+    <>
+      <When start={date} end={date} whole_day={true} open_end={false} />
+    </>
+  ) : null;
+};
+
+const SubjectTags = (props) => {
+  const tags = props.content?.subjects;
+  return tags ? (
+    <>
+      Filed under:{' '}
+      {tags.map((tag) => (
+        <Label key={tag}>{tag}</Label>
+      ))}
+    </>
+  ) : null;
+};
 
 function CcaEventView(props) {
   const {
@@ -21,6 +44,8 @@ function CcaEventView(props) {
         <p>{content.description}</p>
         <HTMLField value={content.text} className="long_description" />
         <RenderBlocks {...props} />
+        {/* <Date {...props} /> */}
+        <SubjectTags {...props} />
       </div>
     </div>
   );
