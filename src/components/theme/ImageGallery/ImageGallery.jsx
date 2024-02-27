97901892059d308a9500f53573ea1f1f0e27a587
kreafox <krisztina.n.elekes@gmail.com>
2024-02-27 18:14:07 +0200

    fix: remove unused import

--- src/components/theme/ImageGallery/ImageGallery.jsx
+++ src/components/theme/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import loadable from '@loadable/component';
 import { Modal, Image } from 'semantic-ui-react';
-import cx from 'classnames';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
