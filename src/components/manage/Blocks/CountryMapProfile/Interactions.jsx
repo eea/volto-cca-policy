e2df968ea4b5dc669868c6a2cc51589353b4481b
Tripon Eugen <tripon.eugen@gmail.com>
2024-02-28 13:06:38 +0200

    country map ol eslint

--- src/components/manage/Blocks/CountryMapProfile/Interactions.jsx
+++ src/components/manage/Blocks/CountryMapProfile/Interactions.jsx
@@ -191,7 +191,7 @@ export const Interactions = ({
       //   onFeatureClick(feature);
       // }
     });
-  }, [map, tooltipRef, onFeatureClick]);
-
+  });
+  // }, [map, tooltipRef, onFeatureClick]);
   return null;
 };
