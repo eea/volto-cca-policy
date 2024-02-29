33bfaf0957d8ab26fef1d47b9f52925ac5a60748
GhitaB <ghitabizau27@gmail.com>
2024-02-29 15:15:45 +0200

    Refs #266057 - Fix event URL in event title when event_url is empty.

--- src/components/manage/Blocks/Listing/EventCardsListingView.jsx
+++ src/components/manage/Blocks/Listing/EventCardsListingView.jsx
@@ -59,6 +59,13 @@ const EventCardsListingView = ({ items, isEditMode, token }) => {
     return contact_info;
   };
 
+  const event_url = (item) => {
+    if (!!item.event_url) {
+      return item.event_url;
+    }
+    return item.id;
+  };
+
   return (
     <div className={cx('ui fluid eventCards')}>
       {items.map((item, index) => (
@@ -76,7 +83,7 @@ const EventCardsListingView = ({ items, isEditMode, token }) => {
                   <Grid.Column width={10}>
                     <div className="event-details">
                       <h3 className={'listing-header'}>
-                        <a href={item.event_url}>
+                        <a href={event_url(item)}>
                           {item.title ? item.title : item.id}
                         </a>
                       </h3>
