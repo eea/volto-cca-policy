6d4751e0750373c1e9dd657dc9346c5590ef38d0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-03-06 16:04:22 +0200

    Add translate button

--- src/components/MigrationButtons.jsx
+++ src/components/MigrationButtons.jsx
@@ -1,54 +1,61 @@
 import { Plug } from '@plone/volto/components/manage/Pluggable';
 import { getBaseUrl } from '@plone/volto/helpers';
 
+const button = (id, title, label, destination) => (
+  <button
+    className={`circle-right-btn `}
+    id={id}
+    onClick={() => window.open(destination, '_blank')}
+    title={title}
+  >
+    {label}
+  </button>
+);
+
 function MigrationButtons(props) {
   const { content, token, pathname } = props;
   const contentId = content?.['@id'] || '';
   const show = !!token && contentId && contentId.indexOf('europa.eu') === -1;
   const base = getBaseUrl(pathname);
+  const buttons = [
+    button(
+      'migration',
+      'Migrate context',
+      'M',
+      `http://localhost:8080/cca/${base}/@@volto_migrate`,
+    ),
+    button(
+      'view',
+      'View original',
+      'V',
+      `https://climate-adapt.eea.europa.eu${base}`,
+    ),
+    button(
+      'debug',
+      'Debug',
+      'PDB',
+      `http://localhost:8080/cca/${base}/@@gopdb`,
+    ),
+    button(
+      'translate',
+      'Translate',
+      'T',
+      `http://localhost:8080/cca/${base}/@@volto-html`,
+    ),
+  ];
 
-  return show ? (
+  if (!show) return null;
+
+  return (
     <Plug
       pluggable="main.toolbar.top"
       id="cca-migration-helpers"
       order={0}
       dependencies={[contentId]}
     >
-      <button
-        className={`circle-right-btn `}
-        id="toolbar-migration"
-        onClick={() =>
-          window.open(
-            `http://localhost:8080/cca/${base}/@@volto_migrate`,
-            '_blank',
-          )
-        }
-        title="Migrate context"
-      >
-        M
-      </button>
-
-      <button
-        className={`circle-right-btn `}
-        id="toolbar-view"
-        onClick={() =>
-          window.open(`https://climate-adapt.eea.europa.eu${base}`, '_blank')
-        }
-        title="View original"
-      >
-        V
-      </button>
-
-      <button
-        className={`circle-right-btn`}
-        id="toolbar-migration"
-        onClick={() => window.open(`http://localhost:8080/cca/${base}/@@gopdb`)}
-        title="Migrate context"
-      >
-        PDB
-      </button>
+      {buttons}
     </Plug>
-  ) : null;
+  );
 }
 
 export default MigrationButtons;
