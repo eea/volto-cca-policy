58e9b0f00dda8858be02c7afeef6652acb3f55ec
kreafox <krisztina.n.elekes@gmail.com>
2024-03-19 13:13:20 +0200

    chore: cleanup, remove unused code

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,4 @@
-import superagent from 'superagent';
 import config from '@plone/volto/registry';
-import { addHeadersFactory } from '@plone/volto/helpers/Proxy/Proxy';
 import { isArray } from 'lodash';
 import { serializeNodes } from '@plone/volto-slate/editor/render';
 import { OBSERVATORY_PARTNERS } from './Constants';
@@ -25,37 +23,6 @@ export {
   EU_COUNTRIES,
 } from './Constants';
 export clientOnly from './clientOnly';
-
-/**
- * Get a resource image/file with authenticated (if token exist) API headers
- * @function getBackendResourceWithAuth
- * @param {Object} req Request object
- * @return {string} The response with the image
- */
-export const getBackendResourceWithAuth = (req) =>
-  new Promise((resolve, reject) => {
-    const { settings } = config;
-
-    let apiPath = '';
-    if (settings.internalApiPath && __SERVER__) {
-      apiPath = settings.internalApiPath;
-    } else if (__DEVELOPMENT__ && settings.devProxyToApiPath) {
-      apiPath = settings.devProxyToApiPath;
-    } else {
-      apiPath = settings.apiPath;
-    }
-    const backendURL = `${apiPath}${req.path}`;
-    const request = superagent
-      .get(backendURL)
-      .maxResponseSize(settings.maxResponseSize)
-      .responseType('blob');
-    const authToken = req.universalCookies.get('auth_token');
-    if (authToken) {
-      request.set('Authorization', `Bearer ${authToken}`);
-    }
-    request.use(addHeadersFactory(req));
-    request.then(resolve).catch(reject);
-  });
 
 export const createSlateParagraph = (text) => {
   return isArray(text) ? text : config.settings.slate.defaultValue();
