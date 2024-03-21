1d161e9a8fe8d6a3734d6d5b8399bb547be8d530
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-03-21 07:53:39 +0200

    Fix sync translation link

--- src/components/MigrationButtons.jsx
+++ src/components/MigrationButtons.jsx
@@ -1,5 +1,5 @@
 import { Plug } from '@plone/volto/components/manage/Pluggable';
-import { getBaseUrl, toPublicURL } from '@plone/volto/helpers';
+import { getBaseUrl, toPublicURL, addAppURL } from '@plone/volto/helpers';
 
 const button = (id, title, label, destination) => (
   <button
@@ -19,51 +19,45 @@ function MigrationButtons(props) {
   const base = getBaseUrl(pathname);
   let buttons = [
     button(
-      'migration',
-      'Migrate context',
-      'M',
-      `http://localhost:8080/cca/${base}/@@volto_migrate`,
-    ),
-    button(
-      'view',
-      'View original',
-      'V',
-      `https://climate-adapt.eea.europa.eu${base}`,
-    ),
-    button(
-      'debug',
-      'Debug',
-      'PDB',
-      `http://localhost:8080/cca/${base}/@@gopdb`,
-    ),
-    button(
-      'translate',
-      'Translate',
-      'T',
-      `http://localhost:8080/cca/${base}/@@volto-html`,
-    ),
-    button(
-      'preview',
-      'Preview Translation',
-      'P',
-      `http://localhost:8080/cca/${base}/@@volto-html?half=1`,
-    ),
-    button(
       'sync-translations',
       'Sync Translations',
       'RT',
-      toPublicURL(`${base}/@@translate-this-async`),
+      addAppURL(toPublicURL(`${base}/@@translate-this-async`)),
     ),
   ];
-
-  if (contentId.indexOf('europa.eu') > -1) {
+  if (contentId.indexOf('europa.eu') === -1) {
     buttons = [
       button(
-        'sync-translations',
-        'Sync Translations',
-        'T',
-        `/${base}/@@translate-this-async`,
+        'migration',
+        'Migrate context',
+        'M',
+        `http://localhost:8080/cca/${base}/@@volto_migrate`,
       ),
+      button(
+        'view',
+        'View original',
+        'V',
+        `https://climate-adapt.eea.europa.eu${base}`,
+      ),
+      button(
+        'debug',
+        'Debug',
+        'PDB',
+        `http://localhost:8080/cca/${base}/@@gopdb`,
+      ),
+      button(
+        'translate',
+        'Translate',
+        'T',
+        `http://localhost:8080/cca/${base}/@@volto-html`,
+      ),
+      button(
+        'preview',
+        'Preview Translation',
+        'P',
+        `http://localhost:8080/cca/${base}/@@volto-html?half=1`,
+      ),
+      ...buttons,
     ];
   }
 
