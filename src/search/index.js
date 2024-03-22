6fa828e1979b1bed51f73f0f24c253cfcf7365ff
Tripon Eugen <tripon.eugen@gmail.com>
2024-03-22 09:49:38 +0200

    Refs #267596 - elastic score update

--- src/search/index.js
+++ src/search/index.js
@@ -3,6 +3,26 @@ import installHealthSearch from './health_observatory/config-health';
 import installMissionStoriesSearch from './mission_stories/config-stories';
 import installMissionProjectsSearch from './mission_projects/config-projects';
 
+const extraQueryParams = {
+  text_fields: [
+    'title^4',
+    'subject^1.5',
+    'description^1.5',
+    'all_fields_for_freetext',
+  ],
+  functions: [
+    {
+      exp: {
+        'issued.date': {
+          offset: '1800d',
+          scale: '3600d',
+        },
+      },
+    },
+  ],
+  score_mode: 'sum',
+};
+
 const applyConfig = (config) => {
   config.settings.searchlib = installHealthSearch(
     installMainSearch(config.settings.searchlib),
@@ -16,7 +36,8 @@ const applyConfig = (config) => {
     installMainSearch(config.settings.searchlib),
   );
 
+  config.settings.searchlib.searchui.ccaSearch.extraQueryParams = extraQueryParams;
+
   return config;
 };
-
 export default applyConfig;
