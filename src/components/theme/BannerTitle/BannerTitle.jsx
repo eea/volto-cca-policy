868d45d8652935913a9a054e6ce897a1013c5fe6
kreafox <krisztina.n.elekes@gmail.com>
2024-03-29 15:37:11 +0200

    fix: banner title component in case of homepage

--- src/components/theme/BannerTitle/BannerTitle.jsx
+++ src/components/theme/BannerTitle/BannerTitle.jsx
@@ -1,37 +1,48 @@
+import React from 'react';
 import config from '@plone/volto/registry';
 import { hasTypeOfBlock } from '@eeacms/volto-cca-policy/utils';
 
 const BannerTitle = (props) => {
   const { content } = props;
   const { blocks } = content;
+  const type = content['@type'];
   const {
     blocks: { blocksConfig },
   } = config;
+  const contentTypes = ['Subsite', 'LRF', 'Plone Site'];
   const TitleBlockView = blocksConfig?.title?.view;
   const hasTitleBlock = hasTypeOfBlock(blocks, '@type', 'title');
   const hasCountryFlagBlock = hasTypeOfBlock(blocks, '@type', 'countryFlag');
-  const types = ['Subsite', 'LRF', 'Plone Site'];
-  const isHomePage = types.indexOf(content?.['@type']) > -1;
+  const [hasBodyClass, setHasBodyClass] = React.useState(false);
+  const isHomePage = contentTypes.indexOf(type) > -1 || hasBodyClass;
 
-  return !isHomePage ? (
+  React.useEffect(() => {
+    const bodyClasses = document.body.className;
+    const hasHomePageClass = bodyClasses.includes('homepage');
+    setHasBodyClass(hasHomePageClass);
+  }, []);
+
+  return isHomePage ? null : (
     <>
       {!hasTitleBlock && !hasCountryFlagBlock ? (
-        <TitleBlockView
-          {...props}
-          data={{
-            info: [{ description: '' }],
-            hideContentType: false,
-            hideCreationDate: false,
-            hideModificationDate: false,
-            hidePublishingDate: false,
-            hideDownloadButton: false,
-            hideShareButton: false,
-          }}
-          metadata={content}
-        />
+        <>
+          <TitleBlockView
+            {...props}
+            data={{
+              info: [{ description: '' }],
+              hideContentType: false,
+              hideCreationDate: false,
+              hideModificationDate: false,
+              hidePublishingDate: false,
+              hideDownloadButton: false,
+              hideShareButton: false,
+            }}
+            metadata={content}
+          />
+        </>
       ) : null}
     </>
-  ) : null;
+  );
 };
 
 export default BannerTitle;
