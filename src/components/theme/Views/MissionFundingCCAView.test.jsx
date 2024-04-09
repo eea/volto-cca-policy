4d27c61f18d486bab47ef77e80dd152db223eda5
GhitaB <ghitabizau27@gmail.com>
2024-04-09 12:11:57 +0300

    Refs #267791 - Fix test.

--- src/components/theme/Views/MissionFundingCCAView.test.jsx
+++ src/components/theme/Views/MissionFundingCCAView.test.jsx
@@ -10,7 +10,70 @@ const mockStore = configureStore();
 
 describe('MissiongFundingCCAView', () => {
   it('should render the component', () => {
-    const data = {};
+    const data = {
+      content: {
+        contributors: [],
+        country: [
+          {
+            title: 'Albania',
+            token: 'AL',
+          },
+          {
+            title: 'Armenia',
+            token: 'AM',
+          },
+        ],
+        description: 'Summary',
+        eligible_entities: [
+          {
+            title: 'Social, cultural, educational bodies',
+            token: 'B_SOCIAL_CULTURAL',
+          },
+          {
+            title: 'NGOs',
+            token: 'D_NGOS',
+          },
+        ],
+        funding_rate: 'funding rate',
+        funding_type: {
+          'content-type': 'text/html',
+          data: '<p>type of funding</p>',
+          encoding: 'utf8',
+        },
+        further_info: 'further info',
+        general_info: '/sandbox',
+        is_blended: true,
+        is_consortium_required: false,
+        objective: {
+          'content-type': 'text/html',
+          data: '<p></p>',
+          encoding: 'utf8',
+        },
+        publication_page: 'https://google.com',
+        rast_steps: [
+          {
+            title: 'Step 1. Preparing the ground for adaptation',
+            token: 'STEP_1',
+          },
+          {
+            title: 'Step 4. Assessing and selecting adaptation options',
+            token: 'STEP_4',
+          },
+        ],
+        regions: 'region here',
+        sectors: [
+          {
+            title: 'Biodiversity',
+            token: 'BIODIVERSITY',
+          },
+          {
+            title: 'Coastal areas',
+            token: 'COASTAL',
+          },
+        ],
+        title: 'Title here',
+      },
+    };
 
     const store = mockStore({
       userSession: { token: '1234' },
