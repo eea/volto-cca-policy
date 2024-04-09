62afa4305837bb02af62c15551f524a76eb19a58
GhitaB <ghitabizau27@gmail.com>
2024-04-09 11:56:55 +0300

    Refs #267791 - Solve disclaimer position, add styles.

--- src/components/theme/Views/MissionFundingCCAView.jsx
+++ src/components/theme/Views/MissionFundingCCAView.jsx
@@ -1,6 +1,7 @@
 import { HTMLField, BannerTitle } from '@eeacms/volto-cca-policy/helpers';
 import { Container, Grid } from 'semantic-ui-react';
 import { PortalMessage } from '@eeacms/volto-cca-policy/components';
+import './styles.less';
 
 function MissiongFundingCCAView(props) {
   const { content } = props;
@@ -91,21 +92,6 @@ function MissiongFundingCCAView(props) {
                   <p>{content.further_info}</p>
                 </>
               )}
-
-              <div class="disclaimer">
-                <h5>Disclaimer</h5>
-                <p>
-                  The contents and links to third-party items on this Mission
-                  webpage are developed by the MIP4Adapt team led by Ricardo,
-                  under contract CINEA/2022/OP/0013/S12.884597 funded by the
-                  European Union and do not necessarily reflect those of the
-                  European Union, CINEA, or those of the European Environment
-                  Agency (EEA) as host of the Climate-ADAPT Platform. Neither
-                  the European Union nor CINEA nor the EEA accepts
-                  responsibility or liability arising out of or in connection
-                  with the information on these pages.
-                </p>
-              </div>
             </Grid.Column>
             <Grid.Column
               mobile={12}
@@ -161,6 +147,37 @@ function MissiongFundingCCAView(props) {
                     </ul>
                   </>
                 )}
+              </div>
+            </Grid.Column>
+          </div>
+        </Grid>
+        <Grid columns="12">
+          <div className="row">
+            <Grid.Column
+              mobile={12}
+              tablet={12}
+              computer={12}
+              className="col-full"
+            >
+              <div className="styled-slate has--style_name--content-box-gray styled">
+                <div className="disclaimer content-box">
+                  <div className="content-box-inner">
+                    <p>
+                      <strong>Disclaimer</strong>
+                      <br />
+                      The contents and links to third-party items on this
+                      Mission webpage are developed by the MIP4Adapt team led by
+                      Ricardo, under contract CINEA/2022/OP/0013/SI2.884597
+                      funded by the European Union and do not necessarily
+                      reflect those of the European Union, CINEA, or those of
+                      the European Environment Agency (EEA) as host of the
+                      Climate-ADAPT Platform. Neither the European Union nor
+                      CINEA nor the EEA accepts responsibility or liability
+                      arising out of or in connection with the information on
+                      these pages.
+                    </p>
+                  </div>
+                </div>
               </div>
             </Grid.Column>
           </div>
