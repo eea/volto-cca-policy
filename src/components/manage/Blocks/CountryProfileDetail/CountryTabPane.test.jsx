2be3ddacc206a08c027c8a3d1d38c8e956c2a976
Tripon Eugen <tripon.eugen@gmail.com>
2024-04-12 12:29:34 +0300

    Refs #161494 - split code and add test

---
+++ src/components/manage/Blocks/CountryProfileDetail/CountryTabPane.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import CountryTabPane from './CountryTabPane';
+
+const mockStore = configureStore();
+
+describe('CountryTabPane', () => {
+  const setActivePanes = () => {
+    return 0;
+  };
+  it('should render the component', () => {
+    const data = {
+      _index: 2,
+      contents: [],
+      activePanes: 1,
+      setActivePanes: setActivePanes,
+    };
+
+    const store = mockStore({
+      userSession: { token: '1234' },
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <CountryTabPane {...data} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(container).toBeTruthy();
+  });
+});
