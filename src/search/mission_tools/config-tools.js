96f2c205100ae3df652429ffaea833fb2aabb7b3
kreafox <krisztina.n.elekes@gmail.com>
2024-04-30 13:13:44 +0300

    feat(mission): add mission_tool search config

---
+++ src/search/mission_tools/config-tools.js
@@ -0,0 +1,57 @@
+import { mergeConfig } from '@eeacms/search';
+import { getClientProxyAddress, getSearchThumbUrl } from '../utils';
+import facets from './facets-tools';
+
+const missionToolsConfig = {
+  title: 'Mission Tools Search',
+  ...facets,
+};
+
+export default function installMissionToolsSearch(config) {
+  const envConfig = process.env.RAZZLE_ENV_CONFIG
+    ? JSON.parse(process.env.RAZZLE_ENV_CONFIG)
+    : missionToolsConfig;
+
+  const pjson = require('@eeacms/volto-cca-policy/../package.json');
+  envConfig.app_name = pjson.name;
+  envConfig.app_version = pjson.version;
+
+  config.searchui.missionToolsSearch = {
+    ...mergeConfig(envConfig, config.searchui.globalsearchbase),
+    elastic_index: '_es/globalsearch',
+    index_name: 'data_searchui',
+    host: process.env.RAZZLE_ES_PROXY_ADDR || 'http://localhost:3000',
+    vocab: {
+      cluster_name: {
+        cca: 'Mission Portal',
+      },
+    },
+  };
+
+  const { missionToolsSearch } = config.searchui;
+
+  missionToolsSearch.facets = facets;
+
+  if (typeof window !== 'undefined') {
+    config.searchui.missionToolsSearch.host =
+      process.env.RAZZLE_ES_PROXY_ADDR || getClientProxyAddress();
+  }
+
+  missionToolsSearch.resultItemModel = {
+    factory: 'ResultModel',
+    urlField: 'about',
+    titleField: 'title',
+    metatypeField: 'objectProvides',
+    descriptionField: 'description',
+    tagsField: 'topic',
+    issuedField: 'issued',
+    getThumbnailUrl: 'getSearchThumbUrl',
+    getIconUrl: 'getGlobalsearchIconUrl',
+    fallbackThumbUrl:
+      'https://react.semantic-ui.com/images/wireframe/white-image.png',
+  };
+
+  config.resolve.getSearchThumbUrl = getSearchThumbUrl();
+
+  return config;
+}
