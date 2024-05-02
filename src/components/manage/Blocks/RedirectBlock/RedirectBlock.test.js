ae6519239a5420898c27e19a656946eb4ffa9efa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-05-02 11:55:09 +0300

    Add test

---
+++ src/components/manage/Blocks/RedirectBlock/RedirectBlock.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import { RedirectBlockDetails } from './RedirectBlockView';
+
+const mockStore = configureStore();
+
+describe('RedirectBlockDetails', () => {
+  it('should render the component', () => {
+    const data = {
+      items: [
+        {
+          id: 'my-item',
+          title: 'Hello',
+          '@id': '/my-item',
+          '@type': 'Folder',
+          href: '/my-item-href',
+        },
+      ],
+      activeMenu: 1,
+      curent_location: '/my-item-href',
+    };
+
+    const store = mockStore({
+      userSession: { token: '1234' },
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RedirectBlockDetails {...data} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(container.querySelector('.redirect-block')).toBeInTheDocument();
+  });
+});
