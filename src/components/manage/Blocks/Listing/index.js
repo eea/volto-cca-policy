94481197acfce37a46e618449dec085596c59365
kreafox <krisztina.n.elekes@gmail.com>
2024-05-14 16:26:56 +0300

    feat: add dropdown variation for listing

--- src/components/manage/Blocks/Listing/index.js
+++ src/components/manage/Blocks/Listing/index.js
@@ -1,12 +1,27 @@
 import OrganisationCardsListingView from './OrganisationCardsListingView';
 import IndicatorCardsListingView from './IndicatorCardsListingView';
 import EventCardsListingView from './EventCardsListingView';
+import DropdownListingView from './DropdownListingView';
 
 export default function installListing(config) {
   config.blocks.blocksConfig.listing = {
     ...config.blocks.blocksConfig.listing,
     variations: [
       ...config.blocks.blocksConfig.listing.variations,
+      {
+        id: 'dropdown',
+        title: 'Dropdown',
+        template: DropdownListingView,
+        isDefault: false,
+        fullobjects: true,
+        schemaEnhancer: ({ schema }) => {
+          schema.properties.placeholder_text = {
+            title: 'Placeholder text',
+          };
+          schema.fieldsets[0].fields.push('placeholder_text');
+          return schema;
+        },
+      },
       {
         id: 'organisationCards',
         title: 'Organisation Cards',
