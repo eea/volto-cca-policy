7c99ffc6013ebd9a0625acce762f7069648037c7
Tripon Eugen <tripon.eugen@gmail.com>
2024-10-07 13:15:06 +0300

    Refs #272477 - test update

--- src/components/manage/Blocks/C3SIndicatorsListingBlock/C3SIndicatorsListingBlockView.test.jsx
+++ src/components/manage/Blocks/C3SIndicatorsListingBlock/C3SIndicatorsListingBlockView.test.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import C3SIndicatorsListingBlockView from './C3SIndicatorsListingBlockView';
+
 const mockStore = configureStore();
 
 describe('C3SIndicatorsListingBlockView', () => {
   it('should render the component', () => {
     const data = {
-      metadata: [
-        { url: 'https://www.europa.eu', title: 'Europa' },
-        { url: 'https://www.copernicus.eu/en', title: 'Copernicus' },
-      ],
+      metadata: {
+        '@components': {
+          c3s_indicators_listing: {
+            items: [
+              { url: 'https://www.europa.eu', title: 'Europa' },
+              { url: 'https://www.copernicus.eu/en', title: 'Copernicus' },
+            ],
+          },
+        },
+      },
     };
 
     const store = mockStore({
