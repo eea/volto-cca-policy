f7eca29965531fbc6743cfb4d9b2c58b87bf1560
kreafox <krisztina.n.elekes@gmail.com>
2025-01-09 11:07:50 +0200

    feat(views): show weblink on news item view

--- src/components/theme/Views/NewsItemView.jsx
+++ src/components/theme/Views/NewsItemView.jsx
@@ -4,12 +4,21 @@ import { filterBlocks } from '@eeacms/volto-cca-policy/utils';
 import { PortalMessage } from '@eeacms/volto-cca-policy/components';
 import { HTMLField, SubjectTags } from '@eeacms/volto-cca-policy/helpers';
 import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
+import { UniversalLink } from '@plone/volto/components';
 
 const DescriptionText = ({ content }) => {
   return (
     <>
       <p className="documentDescription">{content.description}</p>
       <HTMLField value={content.text} className="content-text" />
+      {content.remoteUrl && (
+        <div className="news-link">
+          <span>Read more on </span>
+          <UniversalLink href={content.remoteUrl}>
+            {content.remoteUrl}
+          </UniversalLink>
+        </div>
+      )}
     </>
   );
 };
