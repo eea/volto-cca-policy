23e5f6e395021504b4a955564eb46425933cf4e5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-02-04 11:47:17 +0200

    Remove console log

--- src/search/common.js
+++ src/search/common.js
@@ -138,7 +138,7 @@ export const language = {
   isMulti: true,
   default: (options) => {
     const { language } = options || {};
-    console.log('options', options);
+    // console.log('options', options);
     return {
       values: [language || 'en'],
       type: 'any',
