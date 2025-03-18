e7cad8aa932373b255c875f14f59a18ef3aa51c0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-03-18 12:17:08 +0200

    Tweaks to functionality of case study explorer

--- src/components/manage/Blocks/CaseStudyExplorer/FeatureDisplay.jsx
+++ src/components/manage/Blocks/CaseStudyExplorer/FeatureDisplay.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Icon } from 'semantic-ui-react';
 
-export default function FeatureDisplay({ feature }) {
+export default function FeatureDisplay({ feature, onClose }) {
   return feature ? (
     <div id="csepopup">
+      <div
+        className="close"
+        role="button"
+        onClick={onClose}
+        tabIndex={-1}
+        onKeyDown={onClose}
+      >
+        <Icon className="ri-close-circle-line" />
+      </div>
       <p>
         <strong>
           <a className="dbitem" href={feature.url} target="_blank">
