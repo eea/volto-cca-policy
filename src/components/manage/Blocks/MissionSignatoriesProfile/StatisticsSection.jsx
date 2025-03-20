3a1ca18aad31813e55c298c4160bd5092eb8303b
kreafox <krisztina.n.elekes@gmail.com>
2025-03-20 16:01:43 +0200

    feat: improve statistic section, add assessment section (WIP) - refs #285361

--- src/components/manage/Blocks/MissionSignatoriesProfile/StatisticsSection.jsx
+++ src/components/manage/Blocks/MissionSignatoriesProfile/StatisticsSection.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
+import {
+  StatisticValue,
+  StatisticLabel,
+  StatisticGroup,
+  Statistic,
+} from 'semantic-ui-react';
+
 const StatisticsSection = ({ statistics }) => {
   return (
-    <div className="ui small two statistics center">
-      {statistics.map((stat, index) => (
-        <div key={index} className="ui statistic">
-          <div className="value slate text-center secondary">
-            <p>{stat.value}</p>
-          </div>
-          <div className="label slate text-center primary">
-            <p>{stat.label}</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <StatisticGroup widths="two" size="small">
+        {statistics.map((stat, index) => (
+          <Statistic key={index}>
+            <StatisticValue>{stat.value}</StatisticValue>
+            <StatisticLabel>{stat.label}</StatisticLabel>
+          </Statistic>
+        ))}
+      </StatisticGroup>
+    </>
   );
 };
 
--- src/components/manage/Blocks/MissionSignatoriesProfile/TabSections/AssessmentTab.jsx
+++ src/components/manage/Blocks/MissionSignatoriesProfile/TabSections/AssessmentTab.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import {
+  Tab,
+  Image,
+  Button,
+  Segment,
+  Grid,
+  GridColumn,
+  Item,
+  ItemGroup,
+  ItemContent,
+} from 'semantic-ui-react';
 import { Callout } from '@eeacms/volto-eea-design-system/ui';
+
+import image from '@eeacms/volto-cca-policy/../theme//assets/images/image-narrow.svg';
 
 const AssessmentTab = () => {
   return (
@@ -12,6 +24,96 @@ const AssessmentTab = () => {
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </Callout>
+
+      <div className="profile-content-box">
+        <h3>Nam tempor finibus lorem</h3>
+        <div className="profile-content-box-info">
+          <ItemGroup unstackable className="row">
+            <Item>
+              <Image size="miny" src={image} />
+              <ItemContent
+                verticalAlign="middle"
+                style={{ paddingLeft: '5px' }}
+              >
+                Lorem ipsum dolor sit amet
+              </ItemContent>
+            </Item>
+          </ItemGroup>
+
+          <div className="date">
+            <p>Year of publication: 2022</p>
+          </div>
+        </div>
+
+        <h4>Nam tempor finibus lorem, nec varius arcu convallis sed</h4>
+
+        <div className="items-wrapper">
+          <Grid className="column-grid">
+            <GridColumn width={6} className="column-blocks-wrapper">
+              <ItemGroup unstackable className="row">
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+              </ItemGroup>
+            </GridColumn>
+
+            <GridColumn width={6}>
+              <ItemGroup unstackable className="row">
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+
+                <Item>
+                  <Image size="small" src={image} />
+                  <ItemContent verticalAlign="middle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </ItemContent>
+                </Item>
+              </ItemGroup>
+            </GridColumn>
+          </Grid>
+        </div>
+
+        <h4>Further details</h4>
+        <Segment>
+          Nam tempor finibus lorem, nec varius arcu convallis sed. Nunc id orci
+          a neque vehicula malesuada. Donec vehicula libero vel leo convallis,
+          nec tincidunt felis tincidunt. Maecenas euismod tristique leo, vel
+          malesuada ligula malesuada sed. Donec eget libero id leo congue
+          venenatis.
+        </Segment>
+        <Button primary inverted>
+          Download
+        </Button>
+      </div>
+
       <p>
         Nam tempor finibus lorem, nec varius arcu convallis sed. Nunc id orci a
         neque vehicula malesuada. Donec vehicula libero vel leo convallis, nec
