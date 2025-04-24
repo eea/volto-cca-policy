c4e484a9ffe87451d718ca3e1c54acafa7293489
kreafox <krisztina.n.elekes@gmail.com>
2025-04-24 14:31:35 +0300

    fix: refactor ContextNavigation component

--- src/components/manage/Blocks/RASTBlock/ContextNavigation.jsx
+++ src/components/manage/Blocks/RASTBlock/ContextNavigation.jsx
@@ -12,17 +12,24 @@ import { useLocation } from 'react-router-dom';
  */
 export function ContextNavigationComponent(props) {
   const { location, items, skip_items, show_subfolders } = props;
-  let activeMenu = null;
+  const [activeMenu, setActiveMenu] = React.useState(null);
+  const curentLocation = useLocation();
 
-  const curent_location = useLocation();
-  for (let i = 0; i < items.length; i++) {
-    let itemUrl = '/' + items[i]['@id'].split('/').slice(3).join('/');
-    items[i].is_active = false;
-    if (curent_location.pathname.includes(itemUrl)) {
-      activeMenu = i;
-      items[i].is_active = true;
+  React.useEffect(() => {
+    let newItems = [...items];
+    let activeIndex = null;
+
+    for (let i = 0; i < newItems.length; i++) {
+      let itemUrl = '/' + newItems[i]['@id'].split('/').slice(3).join('/');
+      newItems[i].is_active = false;
+      if (curentLocation.pathname.includes(itemUrl)) {
+        activeIndex = i;
+        newItems[i].is_active = true;
+      }
     }
-  }
+
+    setActiveMenu(activeIndex);
+  }, [curentLocation, items]);
 
   return (
     <>
@@ -36,7 +43,7 @@ export function ContextNavigationComponent(props) {
         <RASTAccordion
           items={items}
           show_subfolders={show_subfolders}
-          curent_location={curent_location}
+          curent_location={curentLocation}
           activeMenu={activeMenu}
         />
       ) : null}
