ea4b8beed2cb7426c3125b6edb85d3b1fadf66a1
eea-jenkins <eea-github@googlegroups.com>
2025-05-21 10:31:20 +0200

    style: Automated code fix

--- src/index.js
+++ src/index.js
@@ -100,8 +100,8 @@ const applyConfig = (config) => {
   ];
 
   if (!config.settings.loadables.reactTable)
-    config.settings.loadables.reactTable = loadable.lib(
-      () => import('@tanstack/react-table'),
+    config.settings.loadables.reactTable = loadable.lib(() =>
+      import('@tanstack/react-table'),
     );
 
   config.settings.dateLocale = 'en-gb';
@@ -562,8 +562,9 @@ const applyConfig = (config) => {
     // 'content',
   ];
 
-  config.widgets.vocabulary['plone.app.vocabularies.Users'] =
-    SelectAutoCompleteWidget;
+  config.widgets.vocabulary[
+    'plone.app.vocabularies.Users'
+  ] = SelectAutoCompleteWidget;
 
   return compose(installBlocks, installSearchEngine, installStore)(config);
 };
