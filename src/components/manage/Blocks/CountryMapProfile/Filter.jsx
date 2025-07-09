59bc0c7135290cd4edb7faa53dee8a5e31376c57
Tripon Eugen <tripon.eugen@gmail.com>
2025-07-09 09:55:23 +0300

    Refs #289437 - country map profile label translation

--- src/components/manage/Blocks/CountryMapProfile/Filter.jsx
+++ src/components/manage/Blocks/CountryMapProfile/Filter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 export default function Filter(props) {
   return (
@@ -6,16 +7,38 @@ export default function Filter(props) {
       <div className="legend portals-legend">
         <div className="legend-el">
           <span className="country-eea-member legend-box"></span>
-          <p className="legend-text">EEA member countries</p>
+          <p className="legend-text">
+            <FormattedMessage
+              id="EEA member countries"
+              defaultMessage="EEA member countries"
+            />
+          </p>
         </div>
         <div className="legend-el">
           <span className="country-eea-coopereting legend-box"></span>
-          <p className="legend-text">EEA coopereting countries</p>
+          <p className="legend-text">
+            <FormattedMessage
+              id="EEA coopereting countries"
+              defaultMessage="EEA coopereting countries"
+            />
+          </p>
         </div>
         <div className="legend-el">
           <span className="country-eastern legend-box"></span>
           <p className="legend-text">
-            Eastern parnership members of the Energy Community
+            <FormattedMessage
+              id="Eastern parnership members of the Energy Community"
+              defaultMessage="Eastern parnership members of the Energy Community"
+            />
+          </p>
+        </div>
+        <div className="legend-el">
+          <span className="legend-box"></span>
+          <p className="legend-text">
+            <FormattedMessage
+              id="This designation is without prejudice to positions on status, and is in line with UNSCR 1244/99 and the ICJ opinion on Kosovo Declaration of Independence."
+              defaultMessage="This designation is without prejudice to positions on status, and is in line with UNSCR 1244/99 and the ICJ opinion on Kosovo Declaration of Independence"
+            />
           </p>
         </div>
       </div>
