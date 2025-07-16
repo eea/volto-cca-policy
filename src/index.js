461a20409ccf8e3b1baea17ee7727378dbb38e23
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-07-16 15:51:48 +0300

    Use secondary site id

--- src/index.js
+++ src/index.js
@@ -101,8 +101,8 @@ const applyConfig = (config) => {
   ];
 
   if (!config.settings.loadables.reactTable)
-    config.settings.loadables.reactTable = loadable.lib(() =>
-      import('@tanstack/react-table'),
+    config.settings.loadables.reactTable = loadable.lib(
+      () => import('@tanstack/react-table'),
     );
 
   config.settings.dateLocale = 'en-gb';
@@ -563,14 +563,17 @@ const applyConfig = (config) => {
     // 'content',
   ];
 
-  config.widgets.vocabulary[
-    'plone.app.vocabularies.Users'
-  ] = SelectAutoCompleteWidget;
+  config.widgets.vocabulary['plone.app.vocabularies.Users'] =
+    SelectAutoCompleteWidget;
 
   config.settings.matomoTrackerIdFn = (pathname) => {
     return pathname.split('/')[2] === 'mission'
       ? {
           matomoSiteId:
+            typeof window !== 'undefined'
+              ? window.env?.RAZZLE_MATOMO_SITE_ID || '321'
+              : '321',
+          matomoSecondSiteId:
             typeof window !== 'undefined'
               ? window.env?.RAZZLE_MATOMO_MISSION_SITE_ID || '321'
               : '321',
