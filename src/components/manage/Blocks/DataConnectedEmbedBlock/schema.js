fef7eea74480a455ccbd04f8dc39170ca538cb17
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-07-17 18:25:58 +0300

    No console.log

--- src/components/manage/Blocks/DataConnectedEmbedBlock/schema.js
+++ src/components/manage/Blocks/DataConnectedEmbedBlock/schema.js
@@ -5,6 +5,5 @@ export function addLanguageParamField({ schema, intl }) {
     description:
       'The name of the URL search parameter that will be added to the URL. The embedded URL should support ',
   };
-  console.log(schema);
   return schema;
 }
