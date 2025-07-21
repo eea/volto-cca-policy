80f8f9bbc590fc0c10c65514bdba0c67db6de441
Tripon Eugen <tripon.eugen@gmail.com>
2025-07-21 12:15:19 +0300

    Refs #289437 - update legend map

--- src/components/manage/Blocks/CountryMapProfile/Filter.jsx
+++ src/components/manage/Blocks/CountryMapProfile/Filter.jsx
@@ -6,11 +6,20 @@ export default function Filter(props) {
     <>
       <div className="legend portals-legend">
         <div className="legend-el">
+          <span className="country-eea-eu-member legend-box"></span>
+          <p className="legend-text">
+            <FormattedMessage
+              id="EU Member States and EEA Member Countries"
+              defaultMessage="EU Member States and EEA Member Countries"
+            />
+          </p>
+        </div>
+        <div className="legend-el">
           <span className="country-eea-member legend-box"></span>
           <p className="legend-text">
             <FormattedMessage
-              id="EEA member countries"
-              defaultMessage="EEA member countries"
+              id="EEA Member Countries"
+              defaultMessage="EEA Member Countries"
             />
           </p>
         </div>
@@ -18,8 +27,8 @@ export default function Filter(props) {
           <span className="country-eea-coopereting legend-box"></span>
           <p className="legend-text">
             <FormattedMessage
-              id="EEA cooperating countries"
-              defaultMessage="EEA cooperating countries"
+              id="EEA Cooperating Countries and Energy Community Contracting Parties"
+              defaultMessage="EEA Cooperating Countries and Energy Community Contracting Parties"
             />
           </p>
         </div>
@@ -27,8 +36,8 @@ export default function Filter(props) {
           <span className="country-eastern legend-box"></span>
           <p className="legend-text">
             <FormattedMessage
-              id="Eastern parnership members of the Energy Community"
-              defaultMessage="Eastern parnership members of the Energy Community"
+              id="Eastern Parnership Members and Energy Community Contracting Parties"
+              defaultMessage="Eastern Parnership Members and Energy Community Contracting Parties"
             />
           </p>
         </div>
