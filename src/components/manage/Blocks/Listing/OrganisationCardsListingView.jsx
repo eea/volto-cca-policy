7cffbca5cc2d422120b08fd683d5235f368256ce
kreafox <krisztina.n.elekes@gmail.com>
2025-11-17 11:09:53 +0200

    change: remove website and contribution links from organisation cards, use semantic cards - refs #294273

--- src/components/manage/Blocks/Listing/OrganisationCardsListingView.jsx
+++ src/components/manage/Blocks/Listing/OrganisationCardsListingView.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import { CardGroup, CardHeader, CardContent, Card } from 'semantic-ui-react';
 import { UniversalLink } from '@plone/volto/components';
-import { useIntl, defineMessages, FormattedMessage } from 'react-intl';
-import { makeContributionsSearchQuery } from '@eeacms/volto-cca-policy/helpers';
 
 import './styles.less';
 
-const messages = defineMessages({
-  website: {
-    id: 'Web site',
-    defaultMessage: 'Web site',
-  },
-});
-
 const OrganisationCardsListingView = ({ items }) => {
-  const intl = useIntl();
-
   return (
-    <div className="ui fluid four cards">
+    <CardGroup itemsPerRow={4}>
       {items.map((item, index) => (
-        <div className="ui fluid card u-card" key={index}>
-          <div className="content">
-            <div className="header">
-              <UniversalLink className="image" href={item['@id']}>
+        <Card fluid className="u-card" key={index} href={item['@id']}>
+          <CardContent>
+            <CardHeader>
+              <UniversalLink className="image">
                 <div className="img-container">
                   <img
                     src={item['@id'] + '/@@images/logo/preview'}
@@ -31,32 +20,14 @@ const OrganisationCardsListingView = ({ items }) => {
                   ></img>
                 </div>
               </UniversalLink>
-              <UniversalLink
-                className="header-link org-name"
-                href={item['@id']}
-              >
+              <UniversalLink className="header-link org-name">
                 {item.title}
               </UniversalLink>
-              <UniversalLink
-                className="header-link org-site"
-                href={item.websites?.[0] ?? '#'}
-              >
-                {intl.formatMessage(messages.website)}
-              </UniversalLink>
-              <UniversalLink
-                className="header-link org-site"
-                href={makeContributionsSearchQuery(item)}
-              >
-                <FormattedMessage
-                  id="Observatory contributions"
-                  defaultMessage="Observatory contributions"
-                />
-              </UniversalLink>
-            </div>
-          </div>
-        </div>
+            </CardHeader>
+          </CardContent>
+        </Card>
       ))}
-    </div>
+    </CardGroup>
   );
 };
 
