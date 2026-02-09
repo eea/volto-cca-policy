a58c3aa596defc73fbd07ac0a58e05e510627b65
iugin <tripon.eugen@gmail.com>
2026-02-09 10:46:17 +0200

    Refs #296263 - static countries

--- src/components/manage/Blocks/CountryProfileDetail2026/CountryProfileDetail2026View.js
+++ src/components/manage/Blocks/CountryProfileDetail2026/CountryProfileDetail2026View.js
@@ -13,11 +13,13 @@ import MenuSubNational from './MenuSubNational';
 import './styles.less';
 
 export default function CountryProfileDetail2026View(props) {
-  // const dataHtml =
-  //   props?.properties['@components']?.countryprofile2026?.html || '';
   const dataJson =
     props?.properties['@components']?.countryprofile2026?.json || {};
   const countryName = props?.properties['title'];
+  const dataContent =
+    props?.properties['@components']?.countryprofile2026?.content || [];
+  const dataMenu =
+    props?.properties['@components']?.countryprofile2026?.menu || [];
 
   const [activePanes, setActivePanes] = React.useState({});
 
@@ -32,51 +34,80 @@ export default function CountryProfileDetail2026View(props) {
     'Good practices, cooperation and synergies',
     'Sub national adaptation',
   ];
-  menu.forEach((element, index) => {
-    let content = '';
-    switch (index) {
-      case 0:
-        content = <MenuProfile dataJson={dataJson} />;
-        break;
-      case 1:
-        content = <MenuNationalCircumstances dataJson={dataJson} />;
-        break;
-      case 2:
-        content = <MenuAssesment dataJson={dataJson} />;
-        break;
-      case 3:
-        content = <MenuLegalPolicy dataJson={dataJson} />;
-        break;
-      case 4:
-        content = <MenuStrategiesPlansGoals dataJson={dataJson} />;
-        break;
-      case 5:
-        content = <MenuMonitorEvaluation dataJson={dataJson} />;
-        break;
-      case 6:
-        content = <MenuGoodPractices dataJson={dataJson} />;
-        break;
-      case 7:
-        content = <MenuSubNational dataJson={dataJson} />;
-        break;
-      default:
-        content = 'Content' + index.toString();
-    }
-    panes.push({
-      menuItem: element,
-      render: () => (
-        <TabPane>
-          <CountryTabPane
-            _index={index}
-            contents={content}
-            countryName={countryName}
-            activePanes={activePanes}
-            setActivePanes={setActivePanes}
-          ></CountryTabPane>
-        </TabPane>
-      ),
+  if (Object.keys(dataJson).length > 0) {
+    menu.forEach((element, index) => {
+      let content = '';
+      switch (index) {
+        case 0:
+          content = <MenuProfile dataJson={dataJson} />;
+          break;
+        case 1:
+          content = <MenuNationalCircumstances dataJson={dataJson} />;
+          break;
+        case 2:
+          content = <MenuAssesment dataJson={dataJson} />;
+          break;
+        case 3:
+          content = <MenuLegalPolicy dataJson={dataJson} />;
+          break;
+        case 4:
+          content = <MenuStrategiesPlansGoals dataJson={dataJson} />;
+          break;
+        case 5:
+          content = <MenuMonitorEvaluation dataJson={dataJson} />;
+          break;
+        case 6:
+          content = <MenuGoodPractices dataJson={dataJson} />;
+          break;
+        case 7:
+          content = <MenuSubNational dataJson={dataJson} />;
+          break;
+        default:
+          content = 'Content' + index.toString();
+      }
+      panes.push({
+        menuItem: element,
+        render: () => (
+          <TabPane>
+            <CountryTabPane
+              _index={index}
+              contents={content}
+              countryName={countryName}
+              activePanes={activePanes}
+              setActivePanes={setActivePanes}
+            ></CountryTabPane>
+          </TabPane>
+        ),
+      });
     });
-  });
+  }
+  if (Object.keys(dataMenu).length > 0) {
+    dataMenu.map((menu, index) =>
+      panes.push({
+        menuItem: menu,
+        render: () => {
+          const htmlContent = (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: dataContent[index].map((item) => item.value).join(''),
+              }}
+            />
+          );
+          return (
+            <TabPane>
+              <CountryTabPane
+                _index={index}
+                contents={htmlContent}
+                countryName={countryName}
+                activePanes={activePanes}
+                setActivePanes={setActivePanes}
+              ></CountryTabPane>
+            </TabPane>
+          );
+        },
+      }),
+    );
+  }
 
   return (
     <div id="countryProfile2026">
