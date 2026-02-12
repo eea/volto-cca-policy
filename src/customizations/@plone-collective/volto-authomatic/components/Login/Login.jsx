f57bd21a9946d05d93324fffc851092bec35e3e4
kreafox <krisztina.n.elekes@gmail.com>
2026-02-12 10:44:44 +0200

    fix: remove unused import

--- src/customizations/@plone-collective/volto-authomatic/components/Login/Login.jsx
+++ src/customizations/@plone-collective/volto-authomatic/components/Login/Login.jsx
@@ -25,7 +25,6 @@ import { toast } from 'react-toastify';
 import { Toast } from '@plone/volto/components';
 import aheadSVG from '@plone/volto/icons/ahead.svg';
 import clearSVG from '@plone/volto/icons/clear.svg';
-import './Login.less';
 
 // Import authomatic components and actions
 import {
