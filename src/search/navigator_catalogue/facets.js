bfa6267cb2c42b1076ab766917cc4b89b255d1ef
kreafox <krisztina.n.elekes@gmail.com>
2026-07-27 13:06:40 +0300

    chore: i18n

--- src/search/navigator_catalogue/facets.js
+++ src/search/navigator_catalogue/facets.js
@@ -1,10 +1,34 @@
 import { multiTermFacet } from '@eeacms/search';
+import { defineMessages } from 'react-intl';
 import {
   cca_adaptation_sectors,
   cca_climate_impacts,
   geographic_countries,
   language,
 } from './../common';
+
+const messages = defineMessages({
+  adaptationSupportCycleStep: {
+    id: 'Adaptation Support Cycle Step',
+    defaultMessage: 'Adaptation Support Cycle Step',
+  },
+  userGroups: {
+    id: 'User Groups',
+    defaultMessage: 'User Groups',
+  },
+  typeOfOutputs: {
+    id: 'Type of Outputs',
+    defaultMessage: 'Type of Outputs',
+  },
+  typeOfData: {
+    id: 'Type of Data',
+    defaultMessage: 'Type of Data',
+  },
+  licenseStatus: {
+    id: 'License status',
+    defaultMessage: 'License status',
+  },
+});
 
 const facets = [
   cca_adaptation_sectors,
@@ -15,35 +39,35 @@ const facets = [
     field: 'cca_adaptation_support_cycle_step.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Adaptation Support Cycle Step',
+    label: messages.adaptationSupportCycleStep,
     alwaysVisible: false,
   }),
   multiTermFacet({
     field: 'cca_intended_user_groups.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'User Groups',
+    label: messages.userGroups,
     alwaysVisible: false,
   }),
   multiTermFacet({
     field: 'cca_type_of_outputs.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Type of Outputs',
+    label: messages.typeOfOutputs,
     alwaysVisible: false,
   }),
   multiTermFacet({
     field: 'cca_type_of_data.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Type of Data',
+    label: messages.typeOfData,
     alwaysVisible: false,
   }),
   multiTermFacet({
     field: 'cca_license_status.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'License status',
+    label: messages.licenseStatus,
     alwaysVisible: false,
   }),
 ];
--- src/search/navigator_guide/facets.js
+++ src/search/navigator_guide/facets.js
@@ -1,10 +1,18 @@
 import { multiTermFacet } from '@eeacms/search';
+import { defineMessages } from 'react-intl';
 import {
   cca_adaptation_sectors,
   cca_climate_impacts,
   geographic_countries,
   language,
 } from './../common';
+
+const messages = defineMessages({
+  adaptationSupportCycleStep: {
+    id: 'Adaptation Support Cycle Step',
+    defaultMessage: 'Adaptation Support Cycle Step',
+  },
+});
 
 const facets = [
   cca_adaptation_sectors,
@@ -15,7 +23,7 @@ const facets = [
     field: 'cca_adaptation_support_cycle_step.keyword',
     isFilterable: false,
     isMulti: true,
-    label: 'Adaptation Support Cycle Step',
+    label: messages.adaptationSupportCycleStep,
     alwaysVisible: false,
   }),
 ];
