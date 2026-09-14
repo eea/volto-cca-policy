eee226269aea8b3b361729b660a18b00eae8fad7
kreafox <krisztina.n.elekes@gmail.com>
2026-09-14 12:56:31 +0300

    fix: update output label in NavigatorCatalogueCardItem test

--- src/components/Search/NavigatorCatalogue/NavigatorCatalogueCardItem.test.jsx
+++ src/components/Search/NavigatorCatalogue/NavigatorCatalogueCardItem.test.jsx
@@ -130,7 +130,7 @@ describe('NavigatorCatalogueCardItem', () => {
         'Maps and graphs, Reports and decision support, Datasets and indicators',
       ),
     ).toHaveTextContent(
-      'Output: Maps and graphs, Reports and decision support, Datasets and indicators',
+      'Type of output: Maps and graphs, Reports and decision support, Datasets and indicators',
     );
 
     fireEvent.click(screen.getByLabelText('compare'));
